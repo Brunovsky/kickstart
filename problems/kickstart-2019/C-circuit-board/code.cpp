6c01dedfd9bb1b4ff15cece4de8748e7250e765e
Bruno Carvalho <elbrunovsky@gmail.com>
2019-11-11 19:09:19 +0000

    C circuit board

--- problems/kickstart-2019/C-circuit-board/code.cpp
+++ problems/kickstart-2019/C-circuit-board/code.cpp
@@ -4,13 +4,78 @@ using namespace std;
 
 // *****
 
+int R, C, K;
+vector<vector<int>> V;
+vector<vector<int>> range;
+
+inline bool out_of_bounds(const multiset<int>& span, int num) {
+  if (span.empty()) return false;
+  return num - *span.begin() > K || *span.rbegin() - num > K;
+}
+
+void rangify(int r) {
+  int i = 0, j = 0;
+
+  multiset<int> span;
+  span.insert(V[r][i]);
+
+  while (++j < C) {
+    while (out_of_bounds(span, V[r][j])) {
+      range[r][i] = span.size();
+      span.erase(span.find(V[r][i++]));
+    }
+    span.insert(V[r][j]);
+  }
+
+  while (i < C) {
+    range[r][i] = span.size();
+    span.erase(span.find(V[r][i++]));
+  }
+
+  assert(span.empty());
+}
+
+int subcircuit(int r, int c) {
+  int min_range = range[r][c];
+  int rows = 1;
+  int area = min_range;
+
+  while (++r < R) {
+    min_range = min(min_range, range[r][c]);
+    area = max(area, min_range * ++rows);
+  }
+
+  return area;
+}
+
 auto solve() {
-  return 0;
+  for (int r = 0; r < R; ++r) {
+    rangify(r);
+  }
+
+  int area = 1;
+  for (int r = 0; r < R; ++r) {
+    for (int c = 0; c < C; ++c) {
+      area = max(area, subcircuit(r, c));
+    }
+  }
+  // ^ this could be O(RC) instead of O(R^2 C), but I'm lazy
+
+  return area;
 }
 
 // *****
 
-void reparse_test() {}
+void reparse_test() {
+  cin >> R >> C >> K >> ws;
+  V.assign(R, vector<int>(C, 0));
+  range.assign(R, vector<int>(C, 0));
+  for (int r = 0; r < R; ++r) {
+    for (int c = 0; c < C; ++c) {
+      cin >> V[r][c] >> ws;
+    }
+  }
+}
 
 // *****
 
