bca3e8d741fb6d7acd77920f544c57a02ec81a72
Bruno Carvalho <elbrunovsky@gmail.com>
2020-02-20 01:16:44 +0000

    Fix leet and kickstart templates and scripts

--- scripts/kickstart.py
+++ scripts/kickstart.py
@@ -9,7 +9,7 @@ template = sys.argv[1]
 
 
 def read_year():
-    year = input("Problem Year? ")
+    year = input("Problem Year?  ex: 2020\n> ")
     if not re.match("20[0-9]{2}", year):
         print("Bad input year: %s", year)
         return read_year()
@@ -17,7 +17,7 @@ def read_year():
 
 
 def read_round():
-    rnd = input("Problem Round? ")
+    rnd = input("Problem Round?  ex: B\n> ")
     if not re.match("[A-Z]", rnd):
         print("Bad input round: %s", rnd)
         return read_round()
@@ -25,7 +25,7 @@ def read_round():
 
 
 def read_name():
-    name = input("Problem Name? ")
+    name = input("Problem Name?  ex: le-problemo\n> ")
     if not re.match("[a-zA-Z0-9-]+", name):
         print("Bad input name: %s", name)
         return read_name()
@@ -33,7 +33,7 @@ def read_name():
 
 
 def read_friendly():
-    friendly = input("Problem Friendly Name (human readable)? ")
+    friendly = input("Problem Friendly Name?  ex: Le Problemo\n> ")
     if not re.match("[a-zA-Z0-9-_!?#)(=~+\-*/.:,; ]", friendly):
         print("Bad input friendly name: %s", friendly)
         return read_friendly()
@@ -44,16 +44,20 @@ year = read_year()
 rnd = read_round()
 name = read_name()
 friendly = read_friendly()
-competition = f"kickstart-{year}"
+competition = f"kickstart/{year}"
 problem = f"{rnd}-{name}"
-header = f"Kickstart {year} - {friendly}"
+header = f"# Kickstart {year} - {friendly}\n\n## [{rnd} {friendly}]"
 folder = f"problems/{competition}/{problem}"
 
 os.makedirs(folder, exist_ok=True)
-shutil.copytree(f"templates/{template}", folder, dirs_exist_ok=True)
 
 readme = open(f"{folder}/README.md", "w")
-readme.write(f"# {header}")
+readme.write(header)
 readme.close()
 
-os.chdir(folder)
+# Copy template
+if template == "kickstart-cpp":
+    shutil.copy("templates/kickstart-cpp/code.cpp", folder)
+    shutil.copy("templates/kickstart-cpp/input.txt", folder)
+    os.symlink("../../../../templates/kickstart-cpp/Makefile",
+               f"{folder}/Makefile")
--- scripts/leet.py
+++ scripts/leet.py
@@ -9,7 +9,7 @@ template = sys.argv[1]
 
 
 def read_difficulty():
-    difficulty = input("Problem Difficulty? ")
+    difficulty = input("Problem Difficulty?  ex: medium\n> ")
     if not re.match("[a-z]+", difficulty):
         print("Bad input difficulty: %s", difficulty)
         return read_difficulty()
@@ -17,7 +17,7 @@ def read_difficulty():
 
 
 def read_name():
-    name = input("Problem Name? ")
+    name = input("Problem Name?  ex: le-problemo\n> ")
     if not re.match("[a-zA-Z0-9-]+", name):
         print("Bad input name: %s", name)
         return read_name()
@@ -25,25 +25,31 @@ def read_name():
 
 
 def read_friendly():
-    friendly = input("Problem Friendly Name (human readable)? ")
+    friendly = input("Problem Friendly Name?  ex: DD. Le Problemo\n> ")
     if not re.match("[a-zA-Z0-9-_!?#)(=~+\-*/.:,; ]", friendly):
         print("Bad input friendly name: %s", friendly)
         return read_friendly()
     return friendly
 
 
+def read_link():
+    link = input("Problem URL?\n> ")
+    return link
+
+
 difficulty = read_difficulty()
 name = read_name()
 friendly = read_friendly()
+link = read_link()
 competition = f"leet/{difficulty}"
 problem = f"{name}"
-header = f"LeetCode - {difficulty} - {friendly}"
+header = f"# LeetCode - {difficulty}\n\n## [{friendly}]({link})"
 folder = f"problems/{competition}/{problem}"
 
 os.makedirs(folder, exist_ok=True)
 
 readme = open(f"{folder}/README.md", "w")
-readme.write(f"# {header}")
+readme.write(header)
 readme.close()
 
 # Copy template
