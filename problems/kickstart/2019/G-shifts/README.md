cecb434375ffc3aa249ed861860a2e24ecef3ed7
Bruno Carvalho <elbrunovsky@gmail.com>
2020-02-25 19:57:51 +0000

    kickstart: Template G Shifts

---
+++ problems/kickstart/2019/G-shifts/README.md
@@ -0,0 +1,9 @@
+# Kickstart 2019 - Shifts
+
+## [Shifts (20pts, 23pts)](https://codingcompetitions.withgoogle.com/kickstart/round/0000000000050e02/000000000018fd5e)
+
+Unattempted
+
+* Time: 0 hours
+* Complexity: -
+* Memory: -
---
+++ problems/kickstart/2019/G-shifts/code.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+auto solve() {
+  return 0;
+}
+
+// *****
+
+void reparse_test() {}
+
+// *****
+
+int main() {
+  unsigned T;
+  cin >> T >> ws;
+  for (unsigned t = 1; t <= T; ++t) {
+    reparse_test();
+    auto solution = solve();
+    cout << "Case #" << t << ": " << solution << '\n';
+  }
+  return 0;
+}
