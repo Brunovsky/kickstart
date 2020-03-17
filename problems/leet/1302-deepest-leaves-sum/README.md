8a569bc0127bff68fc15c1b2b60c509f894bc391
Bruno Carvalho <bruno.carvalho.feup@gmail.com>
2020-03-17 10:37:29 +0000

    leet: 1302 Deepest Leaves Sum

---
+++ problems/leet/1302-deepest-leaves-sum/README.md
@@ -0,0 +1,8 @@
+# LeetCode - medium
+
+## [1302. Deepest Leaves Sum](https://leetcode.com/problems/deepest-leaves-sum/)
+
+Solved
+
+* Complexity: O(nodes)
+* Memory: O(1)
---
+++ problems/leet/1302-deepest-leaves-sum/code.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+  void dive(TreeNode *node, int node_depth, int &sum, int &sum_depth) {
+    if (node == nullptr)
+      return;
+    dive(node->left, node_depth + 1, sum, sum_depth);
+    dive(node->right, node_depth + 1, sum, sum_depth);
+    if (node_depth < sum_depth)
+      return;
+    if (node_depth == sum_depth)
+      sum += node->val;
+    if (node_depth > sum_depth) {
+      sum = node->val;
+      sum_depth = node_depth;
+    }
+  }
+
+public:
+  int deepestLeavesSum(TreeNode *root) {
+    int sum = 0, sum_depth = 0;
+    dive(root, 0, sum, sum_depth);
+    return sum;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
