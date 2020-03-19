6bad471c3dea642ebd8c44d1f0f1bec1eea301a2
Bruno Carvalho <bruno.carvalho.feup@gmail.com>
2020-03-19 14:51:35 +0000

    leet: 115 Distinct Subsequences

---
+++ problems/leet/115-distinct-subsequences/README.md
@@ -0,0 +1,8 @@
+# LeetCode - hard
+
+## [115. Distinct Subsequences](https://leetcode.com/problems/distinct-subsequences/)
+
+Solved
+
+* Complexity: O(ST)
+* Memory: O(T)
---
+++ problems/leet/115-distinct-subsequences/code.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+class Solution {
+public:
+  int numDistinct(const string &s, const string &t) {
+    int S = s.size();
+    int T = t.size();
+    if (!S || !T)
+      return 0;
+
+    // There may be problems with size_t being too small for this dp.
+    vector<size_t> prev(T + 1, 0);
+    vector<size_t> next(T + 1, 0);
+    prev[T] = next[T] = 1;
+
+    for (int i = S - 1; i >= 0; --i) {
+      for (int j = T - 1; j >= 0; --j) {
+        next[j] = prev[j];
+        if (s[i] == t[j])
+          next[j] += prev[j + 1];
+      }
+
+      swap(prev, next);
+    }
+
+    return prev[0];
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
