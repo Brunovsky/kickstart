ca02c29bdb1ea52f24c2ce30d37ac7c33bf62f77
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-15 22:48:30 +0100

    leet: 1114 Print in Order

---
+++ problems/leet/1114-print-in-order/README.md
@@ -0,0 +1,9 @@
+# LeetCode - easy
+
+## [1114. Print in Order](https://leetcode.com/problems/print-in-order)
+
+Solved
+
+It's a concurrency problem.
+
+- 1 Mutex, 1 CV
---
+++ problems/leet/1114-print-in-order/code.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+class Foo {
+  mutex waiting;
+  condition_variable cv;
+  int index = 1;
+
+public:
+  Foo() {}
+
+  void first(function<void()> printFirst) {
+    unique_lock<mutex> lock(waiting);
+
+    // obviously this is dead code
+    if (index < 1)
+      cv.wait(lock, [&]() { return index == 1; });
+
+    // printFirst() outputs "first". Do not change or remove this line.
+    printFirst();
+
+    ++index;
+    cv.notify_all();
+  }
+
+  void second(function<void()> printSecond) {
+    unique_lock<mutex> lock(waiting);
+    if (index < 2)
+      cv.wait(lock, [&]() { return index == 2; });
+
+    // printSecond() outputs "second". Do not change or remove this line.
+    printSecond();
+
+    ++index;
+    cv.notify_all();
+  }
+
+  void third(function<void()> printThird) {
+    unique_lock<mutex> lock(waiting);
+    if (index < 3)
+      cv.wait(lock, [&]() { return index == 3; });
+
+    // printThird() outputs "third". Do not change or remove this line.
+    printThird();
+
+    ++index;
+    cv.notify_all();
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
