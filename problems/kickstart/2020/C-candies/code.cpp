58b1bc57f5b3348cf731bc1a27495f9159ae7f55
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-16 15:45:48 +0100

    kickstart: Comments and refactor for 2020 C Candies

--- problems/kickstart/2020/C-candies/code.cpp
+++ problems/kickstart/2020/C-candies/code.cpp
@@ -4,17 +4,24 @@ using namespace std;
 
 // *****
 
+/**
+ * Use 0-based exclusive end indexing for everything (as usual)
+ *
+ * The idea: 4 segments trees
+ *
+ *     A            0  1  2  3  4  5  6  7  8  9
+ * STree even       1     1     1     1     1
+ * STree odd           1     1     1     1     1
+ * STree inc_even   0     2     4     6     8
+ * STree inc_odd       1     3     5     7     9
+ *
+ * Then to answer a Query [L, R[ perform 4 range queries in each tree
+ * and get A, B, C, D. The answer will be ±(C - D - (L - 1)(A - B)).
+ */
+
 struct data_t {
     long even = 0, odd = 0, inc_even = 0, inc_odd = 0;
 };
-
-struct node_t {
-    int L, R;
-    data_t data;
-};
-
-int N, P;
-vector<node_t> tree;
 
 data_t add(const data_t &lhs, const data_t &rhs) {
     return data_t{lhs.even + rhs.even, lhs.odd + rhs.odd,
@@ -28,7 +35,17 @@ void update_data(data_t &data, int i, long v) {
     data.inc_odd = (i & 1) ? i * v : 0;
 }
 
-// Pushup update at leaf node i
+// use only one tree in the actual implementation, obviously
+struct node_t {
+    int L, R;
+    data_t data;
+};
+
+// as usual, P is the highest power of 2 leq 2N.
+int N, P;
+vector<node_t> tree;
+
+// Pushup update at tree leaf node i
 void pushup_update(int i) {
     assert(i >= N);
     while ((i >>= 1) > 0) {
@@ -36,13 +53,14 @@ void pushup_update(int i) {
     }
 }
 
+// Update a value at array index i and pushup the update in the tree
 void update(int ai, long v) {
     int ti = ai < (2 * N - P) ? P + ai : P + ai - N;
     update_data(tree[ti].data, ai, v);
     pushup_update(ti);
 }
 
-// Query the sum in the range [L, R] (exclusive in R despite notation)
+// Perform all range queries in the range [L, R[
 data_t range_query(int i, int L, int R) {
     auto iL = tree[i].L, iR = tree[i].R;
 
@@ -57,6 +75,7 @@ data_t range_query(int i, int L, int R) {
     return add(range_query(i << 1, L, R), range_query(i << 1 | 1, L, R));
 }
 
+// Setup the tree after population the array-aligned leaves with data and ranges
 void pushup_prepare(int i) {
     if (i >= N)
         return;
@@ -86,12 +105,11 @@ auto solve() {
         update_data(tree[i + N].data, i, A[i]);
     }
 
-    // align leaves to tree
+    // realign leaves from array to tree
+    // [leaftbegin, leafend[ is the leaves range in tree
     auto leafbegin = tree.begin() + N, leafend = tree.end();
     rotate(leafbegin, leafbegin + (2 * N - P), leafend);
     pushup_prepare(1);
-
-    auto dt = tree[1].data;
 
     long sum = 0;
 
@@ -102,17 +120,15 @@ auto solve() {
             int i;
             long v;
             cin >> i >> v;
-            // ranges are 0-indexed
             update(i - 1, v);
         }
         if (operation == "Q") {
             int L, R;
             cin >> L >> R;
-            // ranges are 0-indexed exclusive end
             data_t data = range_query(1, --L, R);
-            auto A = data.even, B = data.odd;
-            auto C = data.inc_even, D = data.inc_odd;
-            auto value = (C - D - (L - 1) * (A - B));
+            auto a = data.even, b = data.odd;
+            auto c = data.inc_even, d = data.inc_odd;
+            auto value = (c - d - (L - 1) * (a - b));
             auto query = (L & 1) ? -value : value;
             sum += query;
         }
