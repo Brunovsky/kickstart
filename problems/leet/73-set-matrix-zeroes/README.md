42d76edd95d95cfab067a3e1d284ab934b2f08a4
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-21 23:11:17 +0100

    leet: 73 Set Matrix Zeroes

---
+++ problems/leet/73-set-matrix-zeroes/README.md
@@ -0,0 +1,8 @@
+# LeetCode - medium
+
+## [73. Set Matrix Zeroes](https://leetcode.com/problems/set-matrix-zeroes)
+
+Solved
+
+* Complexity: O(nm)
+* Memory: O(1)
---
+++ problems/leet/73-set-matrix-zeroes/code.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+class Solution {
+public:
+  void setZeroes(vector<vector<int>> &matrix) {
+    size_t n = matrix.size(), m = matrix[0].size();
+    bool i0 = false, j0 = false;
+    for (size_t i = 0; i < n; ++i) {
+      if (matrix[i][0])
+        continue;
+      j0 = true;
+      break;
+    }
+    for (size_t j = 0; j < m; ++j) {
+      if (matrix[0][j])
+        continue;
+      i0 = true;
+      break;
+    }
+    for (size_t i = 1; i < n; ++i) {
+      for (size_t j = 1; j < m; ++j) {
+        if (matrix[i][j])
+          continue;
+        matrix[0][j] = matrix[i][0] = 0;
+      }
+    }
+    for (size_t i = 1; i < n; ++i) {
+      if (matrix[i][0])
+        continue;
+      for (size_t j = 1; j < m; ++j)
+        matrix[i][j] = 0;
+    }
+    for (size_t j = 1; j < m; ++j) {
+      if (matrix[0][j])
+        continue;
+      for (size_t i = 1; i < n; ++i)
+        matrix[i][j] = 0;
+    }
+    if (j0)
+      for (size_t i = 0; i < n; ++i)
+        matrix[i][0] = 0;
+    if (i0)
+      for (size_t j = 0; j < m; ++j)
+        matrix[0][j] = 0;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
