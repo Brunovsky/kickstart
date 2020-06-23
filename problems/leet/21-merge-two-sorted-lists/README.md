954cd736808883d6df5cb186ae7723745efc8947
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-23 15:37:28 +0100

    leet: 21 Merge Two Sorted Lists

---
+++ problems/leet/21-merge-two-sorted-lists/README.md
@@ -0,0 +1,8 @@
+# LeetCode - easy
+
+## [21. Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists)
+
+Solved
+
+* Complexity: O(N)
+* Memory: O(1)
---
+++ problems/leet/21-merge-two-sorted-lists/code.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    if (!l1 || !l2)
+      return l1 ? l1 : l2;
+    if (l1->val > l2->val)
+      swap(l1, l2);
+    ListNode *result = l1, *start = result;
+    l1 = l1->next;
+    while (l1 && l2) {
+      ListNode *&min = l1->val <= l2->val ? l1 : l2;
+      result = result->next = min;
+      min = min->next;
+    }
+    result->next = l1 ? l1 : l2;
+    return start;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
