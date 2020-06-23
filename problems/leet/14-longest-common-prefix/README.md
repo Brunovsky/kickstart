e8a071e2c59f66da8362f5056b0eb897f727f9e3
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-23 14:19:23 +0100

    leet: 14 Longest Common Prefix

---
+++ problems/leet/14-longest-common-prefix/README.md
@@ -0,0 +1,8 @@
+# LeetCode - easy
+
+## [14. Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix)
+
+Solved
+
+* Complexity: O(#chars)
+* Memory: O(1)
---
+++ problems/leet/14-longest-common-prefix/code.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+class Solution {
+public:
+  void updatePrefix(string &prefix, const string &str) const {
+    size_t i = 0, len = min(prefix.length(), str.length());
+    while (i < len && prefix[i] == str[i])
+      ++i;
+    prefix.resize(i);
+  }
+
+  string longestCommonPrefix(const vector<string> &strs) {
+    if (strs.empty())
+      return "";
+    string prefix = strs[0];
+    for (size_t i = 1; i < strs.size(); ++i) {
+      updatePrefix(prefix, strs[i]);
+      if (prefix.empty())
+        break;
+    }
+    return prefix;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
