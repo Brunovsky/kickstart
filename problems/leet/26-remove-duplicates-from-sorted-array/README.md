35f058dd7fc38e4551e3cf22a0da8a00d1b2caaa
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-23 17:18:46 +0100

    leet: 26 Remove Duplicates from Sorted Array

---
+++ problems/leet/26-remove-duplicates-from-sorted-array/README.md
@@ -0,0 +1,8 @@
+# LeetCode - easy
+
+## [26. Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array)
+
+Solved
+
+* Complexity: O(N)
+* Memory: O(1)
---
+++ problems/leet/26-remove-duplicates-from-sorted-array/code.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+class Solution {
+public:
+  int removeDuplicates(vector<int> &nums) {
+    if (nums.empty())
+      return 0;
+    int i = 0;
+    for (size_t j = 1; j < nums.size(); ++j)
+      if (nums[i] != nums[j])
+        nums[++i] = nums[j];
+    return i + 1;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
