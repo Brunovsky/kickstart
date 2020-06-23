b39b0b23596a70a5384855ed43db9b2b576e0797
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-23 23:42:00 +0100

    leet: 68 Text Justification

---
+++ problems/leet/68-text-justification/README.md
@@ -0,0 +1,8 @@
+# LeetCode - hard
+
+## [68. Text Justification](https://leetcode.com/problems/text-justification)
+
+Solved
+
+* Complexity: O(W maxWidth)
+* Memory: -
---
+++ problems/leet/68-text-justification/code.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+class Solution {
+public:
+  vector<string> fullJustify(vector<string> &words, int maxWidth) {
+    vector<string> lines;
+
+    int i = 0, j = 0, w = 0, s = 0, W = words.size();
+    while (j < W) {
+      w = words[j++].length();
+      s = 0;
+      while (j < W && w + s + words[j].length() + 1 <= maxWidth)
+        w += words[j++].length(), ++s;
+
+      string line = words[i++];
+
+      if (j < W) {
+        if (s == 0) {
+          line.append(maxWidth - w, ' ');
+        } else {
+          int Sd = (maxWidth - w) / s, Sm = (maxWidth - w) % s;
+
+          string space = string(Sd, ' ');
+          while (i < j)
+            line.append(Sd + (Sm-- > 0), ' ').append(words[i++]);
+        }
+      } else {
+        while (i < j)
+          line.append(1, ' ').append(words[i++]);
+        line.append(maxWidth - w - s, ' ');
+      }
+
+      lines.push_back(move(line));
+    }
+    return lines;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
