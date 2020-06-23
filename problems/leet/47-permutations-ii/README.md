30dc3d473c203fc1b397ddeb68cdf10a0812d420
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-23 21:02:05 +0100

    leet: 47 Permutations II

---
+++ problems/leet/47-permutations-ii/README.md
@@ -0,0 +1,8 @@
+# LeetCode - medium
+
+## [47. Permutations II](https://leetcode.com/problems/permutations-ii)
+
+Solved
+
+* Complexity: -
+* Memory: -
---
+++ problems/leet/47-permutations-ii/code.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+constexpr size_t compute_factorial(size_t n) {
+  size_t fact = n;
+  while (--n > 1)
+    fact *= n;
+  return fact;
+}
+
+class Solution {
+public:
+  vector<vector<int>> permuteUnique(vector<int> &nums) {
+    size_t i = 0, N = nums.size(), factorial = compute_factorial(N);
+
+    sort(nums.begin(), nums.end());
+
+    while (i < N) {
+      size_t j = i + 1;
+      while (j < N && nums[i] == nums[j])
+        ++j;
+      factorial /= compute_factorial(j - i);
+      i = j;
+    }
+
+    vector<vector<int>> permutations;
+    permutations.reserve(factorial);
+
+    i = 0;
+    while (++i <= factorial) {
+      permutations.push_back(nums);
+      next_permutation(nums.begin(), nums.end());
+    }
+
+    return permutations;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
