f2398f8ba363503538e98220d07eb5ec176809c8
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-24 13:32:23 +0100

    leet: 95 Unique Binary Search Trees II

---
+++ problems/leet/95-unique-binary-search-trees-ii/README.md
@@ -0,0 +1,8 @@
+# LeetCode - medium
+
+## [95. Unique Binary Search Trees II](https://leetcode.com/problems/unique-binary-search-trees-ii)
+
+Solved
+
+* Complexity: See 96
+* Memory: -
---
+++ problems/leet/95-unique-binary-search-trees-ii/code.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+  vector<TreeNode *> generateTreeRange(int min, int max) const {
+    assert(max - min >= -1);
+    if (max - min == -1) {
+      return {nullptr};
+    } else if (max - min == 0) {
+      return {new TreeNode(min)};
+    } else if (max - min == 1) {
+      return {new TreeNode(min, nullptr, new TreeNode(max)),
+              new TreeNode(max, new TreeNode(min), nullptr)};
+    } else {
+      vector<TreeNode *> list;
+      for (int root = min; root <= max; ++root) {
+        vector<TreeNode *> leftList = generateTreeRange(min, root - 1);
+        vector<TreeNode *> rightList = generateTreeRange(root + 1, max);
+        for (TreeNode *left : leftList) {
+          for (TreeNode *right : rightList) {
+            list.push_back(new TreeNode(root, left, right));
+          }
+        }
+      }
+      return list;
+    }
+  }
+
+public:
+  vector<TreeNode *> generateTrees(int n) {
+    if (n == 0)
+      return {};
+    return generateTreeRange(1, n);
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
