ef4c10bfe4487841988aa6258b965526d41927d6
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-24 21:05:36 +0100

    leet: 81 Search in Rotated Sorted Array II

---
+++ problems/leet/81-search-in-rotated-sorted-array-ii/README.md
@@ -0,0 +1,8 @@
+# LeetCode - medium
+
+## [81. Search in Rotated Sorted Array II](https://leetcode.com/problems/search-in-rotated-sorted-array-ii)
+
+Solved
+
+* Complexity: O(N)
+* Memory: O(1)
---
+++ problems/leet/81-search-in-rotated-sorted-array-ii/code.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+class Solution {
+public:
+  bool search(vector<int> &nums, int target) {
+    int N = nums.size(), l = 0, r = N - 1;
+    while (l < r && nums[l] == nums[r])
+      --r;
+    while (l <= r) {
+      int m = (l + r) / 2;
+      if (nums[m] == target)
+        return true;
+      if (nums[l] == nums[m] && nums[m] == nums[r]) {
+        int L = l + 1, R = r - 1;
+        while (L <= R && nums[L] == nums[m])
+          ++L;
+        while (L <= R && nums[m] == nums[R])
+          --R;
+        l = L, r = R;
+      }
+      // by the previous check, this means [l,m] or [m,r] is sorted
+      else if (nums[l] <= nums[m]) {
+        if (nums[l] <= target && target < nums[m])
+          r = m - 1;
+        else
+          l = m + 1;
+      }
+      // by the previous check, this means [l,m] is unsorted, so [m,r] is sorted
+      else /* nums[l] > nums[m] */ {
+        if (nums[l] <= target || target < nums[m])
+          r = m - 1;
+        else
+          l = m + 1;
+      }
+    }
+    return false;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
