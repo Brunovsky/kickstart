7f1cc6565c2b54e156bfeed1b94d5b90ffabd407
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-24 02:37:21 +0100

    leet: 83 Remove Duplicates from Sorted List

---
+++ problems/leet/83-remove-duplicates-from-sorted-list/README.md
@@ -0,0 +1,8 @@
+# LeetCode - easy
+
+## [83. Remove Duplicates from Sorted List](https://leetcode.com/problems/remove-duplicates-from-sorted-list)
+
+Solved
+
+* Complexity: O(N)
+* Memory: O(1)
---
+++ problems/leet/83-remove-duplicates-from-sorted-list/code.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode *deleteDuplicates(ListNode *head) {
+    ListNode *curr = head;
+    while (curr && curr->next) {
+      if (curr->val == curr->next->val) {
+        curr->next = curr->next->next;
+      } else {
+        curr = curr->next;
+      }
+    }
+    return head;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
