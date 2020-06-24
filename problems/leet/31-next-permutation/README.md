d8663a7a5f4580733cdad1e692fbf004f39aeaa1
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-24 19:16:19 +0100

    leet: 31 Next Permutation
    Sorry I'm lazy :(

---
+++ problems/leet/31-next-permutation/README.md
@@ -0,0 +1,8 @@
+# LeetCode - medium
+
+## [31. Next Permutation](https://leetcode.com/problems/next-permutation)
+
+Solved
+
+* Complexity: O(n) worst-case, O(1) average-case
+* Memory: O(1)
---
+++ problems/leet/31-next-permutation/code.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+class Solution {
+public:
+  void nextPermutation(vector<int> &nums) {
+    next_permutation(nums.begin(), nums.end()); // fight me
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
