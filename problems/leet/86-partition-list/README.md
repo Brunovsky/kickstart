6bd095e85c0000aa52347d578df839bda4dd742c
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-24 12:13:14 +0100

    leet: 86 Partition List

---
+++ problems/leet/86-partition-list/README.md
@@ -0,0 +1,8 @@
+# LeetCode - medium
+
+## [86. Partition List](https://leetcode.com/problems/partition-list)
+
+Solved
+
+* Complexity: O(N)
+* Memory: O(1)
---
+++ problems/leet/86-partition-list/code.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode *partition(ListNode *head, int x) {
+    ListNode lesser_prehead(0), greater_prehead(0);
+    ListNode *lesser = &lesser_prehead;
+    ListNode *greater = &greater_prehead;
+    while (head) {
+      if (head->val < x) {
+        lesser = lesser->next = head;
+      } else {
+        greater = greater->next = head;
+      }
+      head = head->next;
+    }
+    greater->next = nullptr;
+    lesser->next = greater_prehead.next;
+    return lesser_prehead.next ?: greater_prehead.next;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
