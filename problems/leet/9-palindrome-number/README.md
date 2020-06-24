29d2347f04911a4b5802c8e9786266a48e0148cd
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-24 15:18:40 +0100

    leet: 9 Palindrome Number

---
+++ problems/leet/9-palindrome-number/README.md
@@ -0,0 +1,8 @@
+# LeetCode - easy
+
+## [9. Palindrome Number](https://leetcode.com/problems/palindrome-number)
+
+Solved
+
+* Complexity: -
+* Memory: -
---
+++ problems/leet/9-palindrome-number/code.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+class Solution {
+public:
+  bool isPalindrome(int x) {
+    return isPalindromeNumeric(x);
+    // return isPalindromeDumb(x);
+  }
+
+  bool isPalindromeDumb(int x) {
+    string str = to_string(x), reversed = str;
+    reverse(reversed.begin(), reversed.end());
+    return str == reversed;
+  }
+
+  bool isPalindromeNumeric(int x) {
+    if (x < 0)
+      return false;
+    int P = 1;
+    while (P <= x / 10)
+      P *= 10;
+    while (x > 9) {
+      int most = x / P, mod = x % P;
+      int least = x % 10;
+      if (most != least)
+        return false;
+      x = mod / 10;
+      P = P / 100;
+    }
+    return P <= 1 || x == 0;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
