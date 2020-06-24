6ce8d44dba8ef535da5a1671d64241cc98aec9db
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-24 12:27:55 +0100

    leet: 90 Subsets II

---
+++ problems/leet/90-subsets-ii/README.md
@@ -0,0 +1,8 @@
+# LeetCode - medium
+
+## [90. Subsets II](https://leetcode.com/problems/subsets-ii)
+
+Solved
+
+* Complexity: O(2^N)
+* Memory: O(2^N)
---
+++ problems/leet/90-subsets-ii/code.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+class Solution {
+public:
+  vector<vector<int>> subsetsWithDup(vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> answers;
+    answers.push_back({});
+
+    int i = 0, N = nums.size();
+    while (i < N) {
+      int num = nums[i];
+      int j = i + 1;
+      while (j < N && num == nums[j])
+        ++j;
+      int count = j - i;
+
+      int A = answers.size();
+      for (int a = 0; a < A; ++a) {
+        vector<int> extended;
+        extended.reserve(answers[a].size() + count);
+        extended = answers[a];
+        for (int c = 1; c <= count; ++c) {
+          extended.push_back(num);
+          answers.push_back(extended);
+        }
+      }
+
+      i = j;
+    }
+
+    return answers;
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
