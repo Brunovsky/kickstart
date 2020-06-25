14b51fb86f7a1faadac257bddc1c3d45ce5679d9
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-25 20:12:20 +0100

    leet: 146 LRU Cache

---
+++ problems/leet/146-lru-cache/README.md
@@ -0,0 +1,8 @@
+# LeetCode - medium
+
+## [146. LRU Cache](https://leetcode.com/problems/lru-cache)
+
+Solved
+
+* Complexity: O(1)
+* Memory: O(N)
---
+++ problems/leet/146-lru-cache/code.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+using list_pt = list<int>::iterator;
+
+class LRUCache {
+  int capacity;
+  list<int> keys;
+  unordered_map<int, pair<list_pt, int>> cache;
+
+public:
+  LRUCache(int capacity) : capacity(capacity) {}
+
+  int get(int key) {
+    auto it = cache.find(key); // amortized O(1)
+    if (it == cache.end()) {
+      return -1;
+    }
+    keys.erase(it->second.first);
+    keys.push_front(key);
+    it->second.first = keys.begin();
+    return it->second.second;
+  }
+
+  void put(int key, int value) {
+    auto it = cache.find(key); // amortized O(1)
+    if (it == cache.end()) {
+      if (keys.size() == capacity) {
+        int old_key = keys.back();
+        keys.pop_back();
+        cache.erase(old_key);
+      }
+      keys.push_front(key);
+      cache[key] = {keys.begin(), value}; // amortized O(1)
+    } else {
+      keys.erase(it->second.first);
+      keys.push_front(key);
+      it->second = {keys.begin(), value};
+    }
+  }
+};
+
+// *****
+
+void test() {
+  cout << "All tests passed \033[1;32m" << u8"\u2713" << "\033[0m\n";
+}
+
+// *****
+
+int main() {
+  test();
+  return 0;
+}
