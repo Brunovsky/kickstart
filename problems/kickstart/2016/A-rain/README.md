06d6e50d4c909c3b62ade52d4faef04294e2cba1
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-28 16:19:33 +0100

    kickstart: 2016 A Rain

---
+++ problems/kickstart/2016/A-rain/README.md
@@ -0,0 +1,9 @@
+# Kickstart 2016 - Rain
+
+## [Rain (9pts, 15pts)](https://codingcompetitions.withgoogle.com/kickstart/round/0000000000201ca2/0000000000201dbb)
+
+Solved
+
+* Time: 6 hours
+* Complexity: O(N log N) with `N = RC`
+* Memory: O(N)
---
+++ problems/kickstart/2016/A-rain/code.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+int R, C;
+int H[50][50];
+int W[50][50];
+bool F[50][50];
+
+struct point {
+    int r, c;
+};
+
+bool operator<(point a, point b) {
+    return tie(W[a.r][a.c], a.r, a.c) < tie(W[b.r][b.c], b.r, b.c);
+}
+
+bool is_sea(point p) {
+    return p.r < 0 || p.c < 0 || p.r >= R || p.c >= C;
+}
+
+const vector<point> adj = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+auto solve() {
+    memset(H, 0, sizeof(H));
+    memset(W, 0, sizeof(W));
+    memset(F, 0, sizeof(F));
+    cin >> R >> C >> ws;
+    if (R <= 2 || C <= 2)
+        return 0;
+    assert(R <= 50 && C <= 50);
+
+    for (int r = 0; r < R; ++r) {
+        for (int c = 0; c < C; ++c) {
+            cin >> H[r][c];
+        }
+    }
+    memcpy(W, H, sizeof(W));
+
+    set<point> valleys;
+
+    for (int r = 0; r < R; ++r) {
+        valleys.insert({r, 0});
+        valleys.insert({r, C - 1});
+        F[r][0] = F[r][C - 1] = true;
+    }
+    for (int c = 0; c < C; ++c) {
+        valleys.insert({0, c});
+        valleys.insert({R - 1, c});
+        F[0][c] = F[R - 1][c] = true;
+    }
+
+    int water = 0;
+    while (!valleys.empty()) {
+        point V = *valleys.begin();
+        valleys.erase(valleys.begin());
+        F[V.r][V.c] = true;
+        int hV = W[V.r][V.c];
+
+        for (point off : adj) {
+            point N = {V.r + off.r, V.c + off.c};
+            if (is_sea(N) || F[N.r][N.c]) {
+                continue;
+            }
+            int hN = W[N.r][N.c];
+            if (hN < hV) {
+                water += hV - hN;
+            }
+            valleys.erase(N);
+            W[N.r][N.c] = max(W[N.r][N.c], hV);
+            valleys.insert(N);
+        }
+    }
+    return water;
+}
+
+// *****
+
+int main() {
+    unsigned T;
+    cin >> T >> ws;
+    for (unsigned t = 1; t <= T; ++t) {
+        auto solution = solve();
+        cout << "Case #" << t << ": " << solution << '\n';
+    }
+    return 0;
+}
