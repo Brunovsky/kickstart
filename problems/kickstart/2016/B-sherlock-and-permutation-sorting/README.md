024eba7239cdcac07d84c4b9605e074d30b51191
Bruno Carvalho <elbrunovsky@gmail.com>
2020-06-30 14:35:02 +0100

    kickstart: 2016 B Sherlock and Permutation Sorting

--- problems/kickstart/2016/B-sherlock-and-permutation-sorting/README.md
+++ problems/kickstart/2016/B-sherlock-and-permutation-sorting/README.md
@@ -2,9 +2,8 @@
 
 ## [Sherlock and Permutation Sorting (19pts, 24pts)](https://codingcompetitions.withgoogle.com/kickstart/round/0000000000201c0c/0000000000201c0d)
 
-Unfinished
-Too slow for test set 2
+Solved
 
-* Time: 50 minutes
-* Complexity: O(N^3)
-* Memory: O(N^2)
+* Time: 5 hours 30 minutes
+* Complexity: O(N^2)
+* Memory: O(N)
--- problems/kickstart/2016/B-sherlock-and-permutation-sorting/code.cpp
+++ problems/kickstart/2016/B-sherlock-and-permutation-sorting/code.cpp
@@ -6,36 +6,43 @@ using namespace std;
 
 using u64 = uint64_t;
 
-u64 N, M;
-vector<vector<u64>> f;
+struct sum_t {
+    u64 one = 0; // c[n][1]
+    u64 C0 = 0;  // SUM_i (c[n][i])          - actually n!
+    u64 C1 = 0;  // SUM_i (2i * c[n][i])
+    u64 C2 = 0;  // SUM_i (i^2 * c[n][i])
+};
 
 auto solve() {
+    u64 N, M;
     cin >> N >> M >> ws;
-    f.assign(N + 1, {});
-    f[0].assign(N + 1, 0);
 
-    for (u64 n = 1; n <= N; ++n) {
-        f[n].assign(n + 1, 0);
-        u64 fac = 1;
-        u64 sum = 0;
-        for (u64 k = 1; k < n; ++k) {
-            fac = (fac * k) % M;
-            for (uint i = 1; i <= n - k; ++i) {
-                // 1 chunk with k numbers + i chunks covering n - k numbers
-                u64 combs = f[k][1] * f[n - k][i];
-                f[n][i + 1] = (f[n][i + 1] + combs) % M;
-                sum = (sum + combs) % M;
-            }
+    vector<sum_t> f(N + 1, sum_t{});
+    f[1] = {1, 1, 2, 1};
+
+    u64 fac = 1;
+
+    for (u64 n = 2; n <= N; ++n) {
+        auto &F = f[n];
+        for (u64 i = 1; i < n; ++i) {
+            const auto &L = f[i];
+            const auto &R = f[n - i];
+            u64 one = L.one;
+            u64 C0 = L.one * (R.C0) % M;
+            u64 C1 = L.one * (R.C1 + 2 * R.C0) % M;
+            u64 C2 = L.one * (R.C2 + R.C1 + R.C0) % M;
+            F.C0 = (F.C0 + C0) % M;
+            F.C1 = (F.C1 + C1) % M;
+            F.C2 = (F.C2 + C2) % M;
         }
         fac = (fac * n) % M;
-        f[n][1] = (fac + M - sum) % M;
+        F.one = (fac + M - F.C0) % M;
+        F.C0 = (F.C0 + F.one) % M;
+        F.C1 = (F.C1 + 2 * F.one) % M;
+        F.C2 = (F.C2 + F.one) % M;
     }
 
-    u64 sum = 0;
-    for (u64 i = 1; i <= N; ++i) {
-        sum = (sum + f[N][i] * i * i) % M;
-    }
-    return sum;
+    return f[N].C2;
 }
 
 // *****
