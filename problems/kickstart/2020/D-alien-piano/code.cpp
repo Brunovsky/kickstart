9cd0162b9801db19abfb2625df6bcf433a77a853
Bruno Carvalho <elbrunovsky@gmail.com>
2020-07-12 09:36:38 +0100

    kickstart: Cleanup 2020 D Alien Piano

--- problems/kickstart/2020/D-alien-piano/code.cpp
+++ problems/kickstart/2020/D-alien-piano/code.cpp
@@ -4,52 +4,49 @@ using namespace std;
 
 // *****
 
-constexpr int min4(int a, int b, int c, int d) {
+inline int min4(int a, int b, int c, int d) {
     return min(min(a, b), min(c, d));
 }
 
 auto solve() {
     int K;
     cin >> K;
-    vector<int> A(K);
-    for (int i = 0; i < K; ++i)
-        cin >> A[i];
 
     int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
 
+    int a;
+    cin >> a;
+
     for (int i = 1; i < K; ++i) {
-        if (A[i - 1] == A[i]) {
-            int m1 = min4(n1, n2 + 1, n3 + 1, n4 + 1);
-            int m2 = min4(n1 + 1, n2, n3 + 1, n4 + 1);
-            int m3 = min4(n1 + 1, n2 + 1, n3, n4 + 1);
-            int m4 = min4(n1 + 1, n2 + 1, n3 + 1, n4);
-            n1 = m1;
-            n2 = m2;
-            n3 = m3;
-            n4 = m4;
+        int b;
+        cin >> b;
+
+        int m1, m2, m3, m4;
+
+        if (a == b) {
+            m1 = min4(n1, n2 + 1, n3 + 1, n4 + 1);
+            m2 = min4(n1 + 1, n2, n3 + 1, n4 + 1);
+            m3 = min4(n1 + 1, n2 + 1, n3, n4 + 1);
+            m4 = min4(n1 + 1, n2 + 1, n3 + 1, n4);
+        }
+        if (a < b) {
+            m1 = min4(n1 + 1, n2 + 1, n3 + 1, n4 + 1);
+            m2 = min4(n1, n2 + 1, n3 + 1, n4 + 1);
+            m3 = min4(n1, n2, n3 + 1, n4 + 1);
+            m4 = min4(n1, n2, n3, n4 + 1);
+        }
+        if (a > b) {
+            m1 = min4(n1 + 1, n2, n3, n4);
+            m2 = min4(n1 + 1, n2 + 1, n3, n4);
+            m3 = min4(n1 + 1, n2 + 1, n3 + 1, n4);
+            m4 = min4(n1 + 1, n2 + 1, n3 + 1, n4 + 1);
         }
 
-        if (A[i - 1] < A[i]) {
-            int m1 = min4(n1 + 1, n2 + 1, n3 + 1, n4 + 1);
-            int m2 = min4(n1, n2 + 1, n3 + 1, n4 + 1);
-            int m3 = min4(n1, n2, n3 + 1, n4 + 1);
-            int m4 = min4(n1, n2, n3, n4 + 1);
-            n1 = m1;
-            n2 = m2;
-            n3 = m3;
-            n4 = m4;
-        }
-
-        if (A[i - 1] > A[i]) {
-            int m1 = min4(n1 + 1, n2, n3, n4);
-            int m2 = min4(n1 + 1, n2 + 1, n3, n4);
-            int m3 = min4(n1 + 1, n2 + 1, n3 + 1, n4);
-            int m4 = min4(n1 + 1, n2 + 1, n3 + 1, n4 + 1);
-            n1 = m1;
-            n2 = m2;
-            n3 = m3;
-            n4 = m4;
-        }
+        a = b;
+        n1 = m1;
+        n2 = m2;
+        n3 = m3;
+        n4 = m4;
     }
 
     return min4(n1, n2, n3, n4);
