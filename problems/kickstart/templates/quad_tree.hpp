c65872936e8dcd6321f484ce32c0321299f48783
Bruno Carvalho <elbrunovsky@gmail.com>
2020-07-19 23:37:35 +0100

    kickstart: Quad tree test refinements

--- problems/kickstart/templates/quad_tree.hpp
+++ problems/kickstart/templates/quad_tree.hpp
@@ -19,18 +19,17 @@ using namespace std;
 
 namespace quad_tree {
 
-using Int = int;
-using Size = size_t;
-
-struct quad_tree;
+constexpr size_t MAX_SIZE_LEAF = 4;
 
 struct Point {
-    Int x, y;
+    int x, y;
 };
 
 struct Box {
     Point min, max;
 };
+
+using PointIt = vector<Point>::iterator;
 
 inline bool operator==(Point lhs, Point rhs) {
     return tie(lhs.x, lhs.y) == tie(rhs.x, rhs.y);
@@ -56,12 +55,6 @@ inline bool contains(Box large, Box small) {
     return ordered(large.min, small.min) && ordered(small.max, large.max);
 }
 
-int quadrant(Point root, Point point) {
-    int dx = root.x <= point.x; // 0 or 1
-    int dy = root.y <= point.y; // 0 or 1
-    return 2 * dx + dy;
-}
-
 bool cmp_x(Point lhs, Point rhs) {
     return lhs.x < rhs.x;
 }
@@ -70,88 +63,175 @@ bool cmp_y(Point lhs, Point rhs) {
     return lhs.y < rhs.y;
 }
 
-void print_box(Box box) {
-    printf("[(%+03d,%+03d) (%+03d,%+03d)]", box.min.x, box.min.y, box.max.x, box.max.y);
+PointIt partition_x(PointIt first, PointIt last, int x) {
+    while (first != last && first->x < x)
+        ++first;
+    if (first == last)
+        return first;
+
+    for (auto it = next(first); it != last; ++it) {
+        if (it->x < x) {
+            iter_swap(it, first);
+            ++first;
+        }
+    }
+    return first;
 }
 
-Int manhat_dist(Point lhs, Point rhs) {
-    return abs(lhs.x - rhs.x) + abs(lhs.y - rhs.y);
-}
+PointIt partition_y(PointIt first, PointIt last, int y) {
+    while (first != last && first->y < y)
+        ++first;
+    if (first == last)
+        return first;
 
-Int euclid_dist(Point lhs, Point rhs) {
-    Int dx = lhs.x - rhs.x;
-    int dy = lhs.y - rhs.y;
-    return dx * dx + dy * dy;
+    for (auto it = next(first); it != last; ++it) {
+        if (it->y < y) {
+            iter_swap(it, first);
+            ++first;
+        }
+    }
+    return first;
 }
 
 struct quad_tree {
-    using quad_tree_ptr = unique_ptr<quad_tree>;
-    quad_tree_ptr children[4];
+    unique_ptr<quad_tree> children[4];
     Box box;
-    Point root;
-    Size size = 0;
-    Size root_count = 0; // how many instances of the root point we have
+    PointIt first, last;
 
-    quad_tree(vector<Point> ps, Box input_box) : box(input_box), size(ps.size()) {
-        assert(ordered(box.min, box.max));
-        assert(size > 0);
-        for (Point point : ps) {
-            assert(bounded(box, point));
+    quad_tree(PointIt first, PointIt last) : first(first), last(last) {
+        box = {{INT_MAX, INT_MAX}, {INT_MIN, INT_MIN}};
+        for (auto it = first; it != last; ++it) {
+            box.min.x = min(box.min.x, it->x);
+            box.min.y = min(box.min.y, it->y);
+            box.max.x = max(box.max.x, it->x);
+            box.max.y = max(box.max.y, it->y);
+        }
+        size_t size = last - first;
+
+        if (size <= MAX_SIZE_LEAF) {
+            return;
         }
 
         auto middle = size / 2;
-        nth_element(ps.begin(), ps.begin() + middle, ps.end(), cmp_x);
-        root.x = ps[middle].x;
-        nth_element(ps.begin(), ps.begin() + middle, ps.end(), cmp_y);
-        root.y = ps[middle].y;
-        box.min = box.max = root;
+        Point root;
+        nth_element(first, first + middle, last, cmp_x);
+        root.x = (first + middle)->x;
+        nth_element(first, first + middle, last, cmp_y);
+        root.y = (first + middle)->y;
 
-        vector<Point> buckets[4] = {};
-        for (Point point : ps) {
-            box.min.x = min(box.min.x, point.x);
-            box.min.y = min(box.min.y, point.y);
-            box.max.x = max(box.max.x, point.x);
-            box.max.y = max(box.max.y, point.y);
-            if (point == root) {
-                root_count++;
-            } else {
-                int b = quadrant(root, point) & 3;
-                buckets[b].push_back(point);
-            }
-        }
-        for (int b = 0; b < 4; b++) {
-            if (!buckets[b].empty()) {
-                auto xmin = (b & 2) ? root.x : box.min.x;
-                auto xmax = (b & 2) ? box.max.x : root.x;
-                auto ymin = (b & 1) ? root.y : box.min.y;
-                auto ymax = (b & 1) ? box.max.y : root.y;
-                Box child_box = {{xmin, ymin}, {xmax, ymax}};
-                children[b] = make_unique<quad_tree>(move(buckets[b]), child_box);
-            }
-        }
+        auto nth2 = partition_x(first, last, root.x);
+        auto nth1 = partition_y(first, nth2, root.y);
+        auto nth3 = partition_y(nth2, last, root.y);
+
+        children[0] = make_unique<quad_tree>(first, nth1);
+        children[1] = make_unique<quad_tree>(nth1, nth2);
+        children[2] = make_unique<quad_tree>(nth2, nth3);
+        children[3] = make_unique<quad_tree>(nth3, last);
     }
 
-    Size count_query(Box query) const noexcept {
-        if (!intersect(query, box)) {
+    size_t count_query(Box query) const noexcept {
+        if (first == last || !intersect(query, box)) {
             return 0;
         }
         if (contains(query, box)) {
-            return size;
+            return last - first;
         }
-        Size count = bounded(query, root) ? root_count : 0;
-        count += children[0] ? children[0]->count_query(query) : 0;
-        count += children[1] ? children[1]->count_query(query) : 0;
-        count += children[2] ? children[2]->count_query(query) : 0;
-        count += children[3] ? children[3]->count_query(query) : 0;
+        size_t count = 0;
+        if (!children[0]) {
+            for (auto it = first; it != last; ++it) {
+                count += bounded(query, *it);
+            }
+        } else {
+            count += children[0]->count_query(query);
+            count += children[1]->count_query(query);
+            count += children[2]->count_query(query);
+            count += children[3]->count_query(query);
+        }
         return count;
     }
 
-    Size depth() const noexcept {
-        Size n0 = children[0] ? children[0]->depth() + 1 : 1;
-        Size n1 = children[1] ? children[1]->depth() + 1 : 1;
-        Size n2 = children[2] ? children[2]->depth() + 1 : 1;
-        Size n3 = children[3] ? children[3]->depth() + 1 : 1;
-        return max(max(n0, n1), max(n2, n3));
+    size_t depth() const noexcept {
+        if (!children[0]) {
+            return 1;
+        } else {
+            auto n0 = children[0]->depth() + 1;
+            auto n1 = children[1]->depth() + 1;
+            auto n2 = children[2]->depth() + 1;
+            auto n3 = children[3]->depth() + 1;
+            return max(max(n0, n1), max(n2, n3));
+        }
+    }
+
+    size_t size() const noexcept {
+        return last - first;
+    }
+};
+
+struct binary_quad_tree {
+    unique_ptr<binary_quad_tree> children[4];
+    Box box;
+    PointIt first, last;
+
+    binary_quad_tree(PointIt first, PointIt last) : first(first), last(last) {
+        box = {{INT_MAX, INT_MAX}, {INT_MIN, INT_MIN}};
+        for (auto it = first; it != last; ++it) {
+            box.min.x = min(box.min.x, it->x);
+            box.min.y = min(box.min.y, it->y);
+            box.max.x = max(box.max.x, it->x);
+            box.max.y = max(box.max.y, it->y);
+        }
+        size_t size = last - first;
+        Point root = {(box.min.x + box.max.x) / 2, (box.min.y + box.max.y) / 2};
+
+        if (size <= MAX_SIZE_LEAF || root == box.min) {
+            return;
+        }
+
+        auto nth2 = partition_x(first, last, root.x);
+        auto nth1 = partition_y(first, nth2, root.y);
+        auto nth3 = partition_y(nth2, last, root.y);
+
+        children[0] = make_unique<binary_quad_tree>(first, nth1);
+        children[1] = make_unique<binary_quad_tree>(nth1, nth2);
+        children[2] = make_unique<binary_quad_tree>(nth2, nth3);
+        children[3] = make_unique<binary_quad_tree>(nth3, last);
+    }
+
+    size_t count_query(Box query) const noexcept {
+        if (first == last || !intersect(query, box)) {
+            return 0;
+        }
+        if (contains(query, box)) {
+            return last - first;
+        }
+        size_t count = 0;
+        if (!children[0]) {
+            for (auto it = first; it != last; ++it) {
+                count += bounded(query, *it);
+            }
+        } else {
+            count += children[0]->count_query(query);
+            count += children[1]->count_query(query);
+            count += children[2]->count_query(query);
+            count += children[3]->count_query(query);
+        }
+        return count;
+    }
+
+    size_t depth() const noexcept {
+        if (!children[0]) {
+            return 1;
+        } else {
+            auto n0 = children[0]->depth() + 1;
+            auto n1 = children[1]->depth() + 1;
+            auto n2 = children[2]->depth() + 1;
+            auto n3 = children[3]->depth() + 1;
+            return max(max(n0, n1), max(n2, n3));
+        }
+    }
+
+    size_t size() const noexcept {
+        return last - first;
     }
 };
 
