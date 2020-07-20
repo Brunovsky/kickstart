103d4693830dc88f1762e2b7efee1e5f013e28e6
Bruno Carvalho <elbrunovsky@gmail.com>
2020-07-20 21:59:32 +0100

    kickstart: Merge fenwick templates

--- problems/kickstart/templates/fenwick.hpp
+++ problems/kickstart/templates/fenwick.hpp
@@ -9,9 +9,9 @@ using namespace std;
 int N;
 int fenwick[MAXN];
 
-int prefix_sum(int i) {
+int sum(int i) {
     int sum = 0;
-    while (i) {
+    while (i > 0) {
         sum += fenwick[i];
         i -= i & -i;
     }
@@ -28,6 +28,39 @@ void add(int i, int n) {
 
 // *****
 
+#define MAXM 500
+
+int M;
+int fenwick2d[MAXN][MAXM];
+
+int sum2(int i, int j) {
+    int sum = 0;
+    while (i > 0) {
+        int k = j;
+        while (k > 0) {
+            sum += fenwick2d[i][k];
+            k -= k & -k;
+        }
+        i -= i & -i;
+    }
+    return sum;
+}
+
+void add2(int i, int j, int n) {
+    assert(i > 0 && j > 0);
+    while (i <= N) {
+        int k = j;
+        while (k <= N) {
+            fenwick2d[i][k] += n;
+            k += k & -k;
+        }
+        i += i & -i;
+    }
+}
+
+// *****
+
 void driver() {
     memset(fenwick, 0, sizeof(fenwick));
+    memset(fenwick2d, 0, sizeof(fenwick2d));
 }
