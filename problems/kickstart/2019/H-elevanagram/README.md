8db55f5dab3953d7972ba5c778a6ce113edb8516
Bruno Carvalho <elbrunovsky@gmail.com>
2020-07-21 19:15:48 +0100

    kickstart: 2019 H Elevanagram

---
+++ problems/kickstart/2019/H-elevanagram/README.md
@@ -0,0 +1,9 @@
+# Kickstart 2019 - Elevanagram
+
+## [Elevanagram (20pts, 27pts)](https://codingcompetitions.withgoogle.com/kickstart/round/0000000000050edd/00000000001a286d)
+
+Solved
+
+* Time: 15 minutes
+* Complexity: O(9 * 11^2)
+* Memory: O(9 * 11^2)
---
+++ problems/kickstart/2019/H-elevanagram/code.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+// Let Ei, Oi be the number of 'i's in even and odd positions. Then:
+// SUM i(Ei - Oi) = 0 (mod 11)  <==>
+// SUM i(Ei - (Ai - Ei)) = 0 (mod 11)  <==>
+// SUM i(2Ei - Ai) = 0 (mod 11)  <==>
+// 2 SUM iEi = SUM iAi (mod 11) <==>
+// SUM iEi = 6 SUM iAi (mod 11)
+
+// dp[i][s][k]: it is possible to find an assignment for E1,...,Ei such that:
+// SUM Ei = s
+// SUM iEi = k (mod 11)
+
+bool dp[10][500][11];
+int A[10];
+
+auto solve() {
+  memset(A, 0, sizeof(A));
+  memset(dp, 0, sizeof(dp));
+
+  for (int i = 1; i <= 9; i++) {
+    cin >> A[i];
+    if (A[i] > 50) {
+      A[i] = 50 + (A[i] & 1);
+    }
+  }
+
+  int Asum = 0;
+  int iAi = 0;
+  dp[0][0][0] = true;
+  for (int i = 1; i <= 9; i++) {
+    for (int Ei = 0; Ei <= A[i]; Ei++) {
+      for (int p = 0; p <= Asum; p++) {
+        for (int c = 0; c <= 10; c++) {
+          dp[i][Ei + p][(c + i * Ei) % 11] |= dp[i - 1][p][c];
+        }
+      }
+    }
+    Asum += A[i];
+    iAi += i * A[i];
+  }
+  return dp[9][(Asum + 1) / 2][(6 * iAi) % 11] ? "YES" : "NO";
+}
+
+// *****
+
+int main() {
+  unsigned T;
+  cin >> T >> ws;
+  for (unsigned t = 1; t <= T; ++t) {
+    auto solution = solve();
+    cout << "Case #" << t << ": " << solution << '\n';
+  }
+  return 0;
+}
