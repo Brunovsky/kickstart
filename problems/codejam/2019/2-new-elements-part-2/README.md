f33ed1ba03c2eb7beecdbf7a5e2e820ce2528b57
Bruno Carvalho <elbrunovsky@gmail.com>
2020-07-26 05:25:12 +0100

    codejam: 2019 2 New Elements: Part 2

--- problems/codejam/2019/2-new-elements-part-2/README.md
+++ problems/codejam/2019/2-new-elements-part-2/README.md
@@ -2,10 +2,8 @@
 
 ## [New Elements: Part 2 (10pts, 16pts)](https://codingcompetitions.withgoogle.com/codejam/round/0000000000051679/0000000000146184)
 
-Partial
+Solved
 
-Precision problems for test set 2
-
-* Time: 5 hours of debugging
-* Complexity: O(N + log^2(10^9))
+* Time: 8 hours
+* Complexity: O(N + log(10^9))
 * Memory: -
--- problems/codejam/2019/2-new-elements-part-2/code.cpp
+++ problems/codejam/2019/2-new-elements-part-2/code.cpp
@@ -14,6 +14,8 @@ long fracgcd(long a, long b) {
 
 struct frac {
     long n, d;
+
+    frac(long num) : n(num), d(1) {}
     frac(long num, long den) : n(num), d(den) {
         if (d < 0) {
             n = -n, d = -d;
@@ -21,7 +23,15 @@ struct frac {
         long g = fracgcd(n, d);
         n /= g, d /= g;
     }
+
+    operator long() const {
+        return d != 0 ? n / d : (n > 0 ? LONG_MAX : LONG_MIN);
+    }
 };
+
+frac inv(const frac& f) {
+    return frac(f.d, f.n);
+}
 
 bool operator==(const frac& a, const frac& b) {
     return a.n == b.n && a.d == b.d;
@@ -30,16 +40,16 @@ bool operator!=(const frac& a, const frac& b) {
     return a.n != b.n || a.d != b.d;
 }
 bool operator<(const frac& a, const frac& b) {
-    return 1.0 * a.n / a.d < 1.0 * b.n / b.d;
+    return a.n * b.d < b.n * a.d;
 }
 bool operator>(const frac& a, const frac& b) {
-    return 1.0 * a.n / a.d > 1.0 * b.n / b.d;
+    return a.n * b.d > b.n * a.d;
 }
 bool operator<=(const frac& a, const frac& b) {
-    return 1.0 * a.n / a.d <= 1.0 * b.n / b.d;
+    return a.n * b.d <= b.n * a.d;
 }
 bool operator>=(const frac& a, const frac& b) {
-    return 1.0 * a.n / a.d >= 1.0 * b.n / b.d;
+    return a.n * b.d >= b.n * a.d;
 }
 
 frac operator+(const frac& a, const frac& b) {
@@ -48,73 +58,30 @@ frac operator+(const frac& a, const frac& b) {
 frac operator-(const frac& a, const frac& b) {
     return frac(a.n * b.d - b.n * a.d, a.d * b.d);
 }
-frac operator*(const frac& a, const frac& b) {
-    return frac(a.n * b.n, a.d * b.d);
-}
-frac operator/(const frac& a, const frac& b) {
-    return frac(a.n * b.d, a.d * b.n);
-}
-frac operator%(const frac& a, const frac& b) {
-    return frac((a.n * b.d) % (b.n * a.d), a.d * b.d);
-}
-
-double absdif(const frac& a, const frac& b) {
-    return abs(1.0 * a.n / a.d - 1.0 * b.n / b.d);
-}
 
 ostream& operator<<(ostream& out, const frac& f) {
     return out << f.n << '/' << f.d;
 }
 
-frac closest(frac f, long maxd) {
-    long p0 = 0, q0 = 1;
-    long p1 = 1, q1 = 0;
-    long n = f.n, d = f.d;
-    if (d <= maxd) {
-        return f;
-    }
-    while (1) {
-        long a = n / d;
-        long p2 = p0 + a * p1;
-        long q2 = q0 + a * q1;
-        if (q2 > maxd) {
-            break;
-        }
-        p0 = p1, q0 = q1;
-        p1 = p2, q1 = q2;
-        n = n - a * d;
-        swap(n, d);
-    }
-    long k = (maxd - q0) / q1;
-    frac b1(p0 + k * p1, q0 + k * q1);
-    frac b2(p1, q1);
-    return absdif(b1, f) <= absdif(b2, f) ? b1 : b2;
-}
-
 #define MAXN 1000
-#define MAXV 1'000'000'100LL
+#define MAXV 1'000'000'100L
 
 int N;
 long A[MAXN];
 long B[MAXN];
 
-frac binary_search(frac lo, frac hi) {
-    long a = lo.n, b = lo.d;
-    long c = hi.n, d = hi.d;
-    frac mid(a * d + b * c, 2 * b * d);
-    // find x,y such that a/b < x/y < c/d
-    long Ly = 1, Ry = b + d;
-    while (Ly <= Ry) {
-        long y = (Ly + Ry) / 2;
-        frac close = closest(mid, y);
-        if (lo < close && close < hi) {
-            Ry = y - 1;
-        } else {
-            Ly = y + 1;
-        }
+frac cf_search(frac lo, frac hi) {
+    assert(lo < hi);
+    long a = lo;
+    if (hi > frac(a + 1)) {
+        return a + 1;
     }
-    long x = a * Ly / b + 1;
-    return frac(x, Ly);
+    frac fa = frac(a);
+    if (lo.d == 1) {
+        return fa + inv(inv(hi - fa) + frac(1));
+    } else {
+        return fa + inv(cf_search(inv(hi - fa), inv(lo - fa)));
+    }
 }
 
 auto solve() {
@@ -122,7 +89,7 @@ auto solve() {
     for (int i = 0; i < N; i++) {
         cin >> A[i] >> B[i];
     }
-    frac lo(1, MAXV);
+    frac lo(0, 1);
     frac hi(MAXV, 1);
     for (int i = 0; i < N; i++) {
         for (int j = i + 1; j < N; j++) {
@@ -141,7 +108,7 @@ auto solve() {
     if (hi <= lo) {
         return "IMPOSSIBLE"s;
     }
-    frac ans = binary_search(lo, hi);
+    frac ans = cf_search(lo, hi);
     assert(lo < ans && ans < hi);
     return to_string(ans.d) + ' ' + to_string(ans.n);
 }
