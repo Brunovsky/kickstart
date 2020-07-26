977767a36d089e1e9daafbd50e8b1bb2966c9b04
Bruno Carvalho <elbrunovsky@gmail.com>
2020-07-26 02:05:42 +0100

    leet: 210 Course Schedule II

---
+++ problems/leet/210-course-schedule-ii/README.md
@@ -0,0 +1,8 @@
+# LeetCode - medium
+
+## [210. Course Schedule II](https://leetcode.com/problems/course-schedule-ii/)
+
+Solved
+
+* Complexity: O(E + V)
+* Memory: O(E + V)
---
+++ problems/leet/210-course-schedule-ii/code.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+class Solution {
+public:
+  vector<int> findOrder(int N, const vector<vector<int>> &prerequisites) {
+    vector<vector<int>> out_adj(N);
+    vector<int> in_adj(N);
+
+    for (uint i = 0; i < prerequisites.size(); i++) {
+      int v = prerequisites[i][0];
+      int u = prerequisites[i][1];
+      out_adj[u].push_back(v);
+      in_adj[v]++;
+    }
+
+    vector<int> frontier;
+    for (int i = 0; i < N; i++) {
+      if (in_adj[i] == 0) {
+        frontier.push_back(i);
+      }
+    }
+
+    vector<int> ans;
+    while (!frontier.empty()) {
+      int u = frontier.back();
+      frontier.pop_back();
+      ans.push_back(u);
+
+      for (int v : out_adj[u]) {
+        in_adj[v]--;
+        if (in_adj[v] == 0) {
+          frontier.push_back(v);
+        }
+      }
+    }
+
+    if (ans.size() != N) {
+      ans.clear();
+    }
+    return ans;
+  }
+};
+
+// *****
+
+int main() {
+  return 0;
+}
