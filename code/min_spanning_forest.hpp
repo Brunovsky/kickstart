fcd1f788f1f9dbc5fa429213f24039ecda0e859b
Bruno Carvalho <elbrunovsky@gmail.com>
2020-07-29 00:06:13 +0100

    code: Test min spanning forest template

--- code/min_spanning_forest.hpp
+++ code/min_spanning_forest.hpp
@@ -8,40 +8,37 @@ using namespace std;
 
 struct min_spanning_forest {
     int V, E;
-    vector<vector<int>> adj;
     vector<int> source;
     vector<int> target;
     vector<int> weight;
 
-    min_spanning_forest(int V) : V(V), E(0) {
-        adj.resize(V, {});
-    }
+    min_spanning_forest(int V) : V(V), E(0) {}
 
     void add(int u, int v, int w) {
         assert(0 <= u && u < V && 0 <= v && v < V && u != v);
-        int e = E++;
-        source.resize(E), target.resize(E), weight.resize(E);
-        source[e] = u;
-        target[e] = v;
-        weight[e] = w;
-        adj[u].push_back(e);
+        source.push_back(u);
+        target.push_back(v);
+        weight.push_back(w);
+        E++;
     }
 
     int compute() {
         disjoint_set set(V);
         vector<int> edges(E);
         iota(begin(edges), end(edges), 0);
-        sort(begin(edges), end(edges), [](int e1, int e2) {
+        sort(begin(edges), end(edges), [this](int e1, int e2) {
             return weight[e1] < weight[e2];
         });
 
-        int total_weight = 0;
+        int msf_weight = 0;
         for (int e : edges) {
-            if (set.find(source[e]) != set.find(target[e])) {
-                total_weight += weight[e];
-                set.join(source[e], target[e]);
+            int u = source[e], v = target[e];
+            if (set.find(u) != set.find(v)) {
+                msf_weight += weight[e];
+                set.join(u, v);
+                // add e to a vec if you want to output the tree's edges
             }
         }
-        return total_weight;
+        return msf_weight;
     }
 };
---
+++ code/test/min_spanning_tree.cpp
@@ -0,0 +1,26 @@
+#include "../min_spanning_forest.hpp"
+
+using namespace std;
+
+// *****
+
+void test() {
+    min_spanning_forest msf(7);
+    msf.add(1, 2, 1);
+    msf.add(1, 4, 4);
+    msf.add(1, 5, 3);
+    msf.add(2, 4, 4);
+    msf.add(2, 5, 2);
+    msf.add(3, 5, 4);
+    msf.add(3, 6, 5);
+    msf.add(4, 5, 4);
+    msf.add(5, 6, 7);
+    int weight = msf.compute();
+    printf("mst weight #1: %d\n", weight);
+    assert(weight == 16);
+}
+
+int main() {
+    test();
+    return 0;
+}
