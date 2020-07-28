dace5c666f06e9846b4e2dad5d4ba81df12ab0ec
Bruno Carvalho <elbrunovsky@gmail.com>
2020-07-28 08:26:33 +0100

    code: More maximum flow!

--- code/maximum_flow.hpp
+++ code/maximum_flow.hpp
@@ -5,19 +5,20 @@ using namespace std;
 // *****
 
 struct maximum_flow {
-    int V, E, s, t;
+    int V, E;
     vector<vector<int>> adj;
     vector<int> source;
     vector<int> target;
     vector<int> cap;
     static constexpr int nil = -1, inf = INT_MAX;
 
-    maximum_flow(int V) : V(V), E(0), s(0), t(V + 1) {
-        adj.resize(V + 2, {});
+    maximum_flow(int V) : V(V), E(0) {
+        adj.resize(V, {});
     }
 
+    // normal nodes are 1..V
     void add(int u, int v, int capacity) {
-        assert(0 <= u && u <= V && 1 <= v && v <= V + 1);
+        assert(0 <= u && u < V && 0 <= v && v < V && u != v);
         int uv = E++;
         int vu = E++;
         source.resize(E), target.resize(E), cap.resize(E);
@@ -29,16 +30,7 @@ struct maximum_flow {
         adj[v].push_back(vu);
     }
 
-    void add_source(int u) {
-        add(s, u, inf);
-    }
-
-    void add_sink(int v) {
-        add(v, t, inf);
-    }
-
-    int compute() {
-        assert(s == 0 && t == V + 1);
+    int compute(int s, int t) {
         vector<int> flow(E, 0);
         int max_flow = 0;
         do {
@@ -64,8 +56,8 @@ struct maximum_flow {
                 df = min(df, cap[e] - flow[e]);
             }
             for (int e = pred[t]; e != nil; e = pred[source[e]]) {
-                flow[e] = flow[e] + df;
-                flow[e ^ 1] = flow[e ^ 1] - df;
+                flow[e] += df;
+                flow[e ^ 1] -= df;
             }
             max_flow += df;
         } while (true);
@@ -73,3 +65,190 @@ struct maximum_flow {
     }
 };
 constexpr int maximum_flow::nil, maximum_flow::inf;
+
+struct maximum_flow_dinic {
+    int V, E;
+    vector<vector<int>> adj;
+    vector<int> source;
+    vector<int> target;
+    vector<int> cap;
+    static constexpr int nil = -1, inf = INT_MAX;
+
+    maximum_flow_dinic(int V) : V(V), E(0) {
+        adj.resize(V, {});
+    }
+
+    // normal nodes are 1..V
+    void add(int u, int v, int capacity) {
+        assert(0 <= u && u < V && 0 <= v && v < V && u != v);
+        int uv = E++;
+        int vu = E++;
+        source.resize(E), target.resize(E), cap.resize(E);
+        source[uv] = target[vu] = u;
+        source[vu] = target[uv] = v;
+        cap[uv] = capacity;
+        cap[vu] = 0;
+        adj[u].push_back(uv);
+        adj[v].push_back(vu);
+    }
+
+    vector<int> flow;
+    vector<int> level;
+    vector<int> arc;
+
+    bool bfs(int s, int t) {
+        level.assign(V + 2, nil);
+        level[s] = 0;
+        queue<int> Q;
+        Q.push(s);
+        while (!Q.empty()) {
+            int u = Q.front();
+            Q.pop();
+            for (int e : adj[u]) {
+                int v = target[e];
+                if (level[v] == nil && cap[e] > flow[e]) {
+                    level[v] = level[u] + 1;
+                    Q.push(v);
+                }
+            }
+        }
+        return level[t] != nil;
+    }
+
+    int dfs(int u, int t, int f) {
+        if (u == t) {
+            return f;
+        }
+        for (int &i = arc[u], end = adj[u].size(); i < end; i++) {
+            int e = adj[u][i];
+            int v = target[e];
+            if (cap[e] > flow[e] && level[u] < level[v]) {
+                int df = dfs(v, t, min(f, cap[e] - flow[e]));
+                if (df > 0) {
+                    flow[e] += df;
+                    flow[e ^ 1] -= df;
+                    return df;
+                }
+            }
+        }
+        return 0;
+    }
+
+    int compute(int s, int t) {
+        flow.assign(E, 0);
+        int max_flow = 0;
+        while (bfs(s, t)) {
+            arc.assign(V, 0);
+            int flow = 0;
+            while ((flow = dfs(s, t, inf))) {
+                max_flow += flow;
+            }
+        }
+        return max_flow;
+    }
+};
+constexpr int maximum_flow_dinic::nil, maximum_flow_dinic::inf;
+
+struct maximum_flow_push_relabel {
+    int V, E;
+    vector<vector<int>> adj;
+    vector<int> source;
+    vector<int> target;
+    vector<int> cap;
+    static constexpr int nil = -1, inf = INT_MAX;
+
+    maximum_flow_push_relabel(int V) : V(V), E(0) {
+        adj.resize(V, {});
+    }
+
+    // normal nodes are 1..V
+    void add(int u, int v, int capacity) {
+        assert(0 <= u && u < V && 0 <= v && v < V && u != v);
+        int uv = E++;
+        int vu = E++;
+        source.resize(E), target.resize(E), cap.resize(E);
+        source[uv] = target[vu] = u;
+        source[vu] = target[uv] = v;
+        cap[uv] = capacity;
+        cap[vu] = 0;
+        adj[u].push_back(uv);
+        adj[v].push_back(vu);
+    }
+
+    vector<int> excess;
+    vector<int> height;
+    vector<int> arc;
+    vector<int> flow;
+    queue<int> active; // fifo selection, O(V^3)
+
+    bool push(int e) {
+        int u = source[e], v = target[e];
+        int send = min(excess[u], cap[e] - flow[e]);
+        flow[e] += send;
+        flow[e ^ 1] -= send;
+        excess[u] -= send;
+        excess[v] += send;
+        if (excess[v] == send) {
+            active.push(v);
+        }
+        return excess[u] > 0;
+    }
+
+    void relabel(int u) {
+        int min_height = inf;
+        for (int e : adj[u]) {
+            if (cap[e] > flow[e]) {
+                min_height = min(min_height, height[target[e]]);
+                height[u] = min_height + 1;
+            }
+        }
+    }
+
+    void discharge(int u) {
+        int vsize = adj[u].size();
+        int &i = arc[u];
+        while (excess[u] > 0) {
+            if (i == vsize) {
+                relabel(u);
+                i = 0;
+            }
+            int e = adj[u][i];
+            int v = target[e];
+            if (cap[e] > flow[e] && height[u] > height[v]) {
+                assert(height[u] == height[v] + 1);
+                i += push(e);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    int compute(int s, int t) {
+        excess.assign(V, 0);
+        height.assign(V, 0);
+        arc.assign(V, 0);
+        flow.assign(E, 0);
+
+        height[s] = V;
+        for (int e : adj[s]) {
+            excess[s] = inf; // recharge s to handle supersources
+            push(e);
+            active.push(target[e]);
+        }
+
+        while (!active.empty()) {
+            int u = active.front();
+            active.pop();
+            if (u != s && u != t) {
+                discharge(u);
+            }
+        }
+
+        int max_flow = 0;
+        for (int e : adj[s]) {
+            max_flow += flow[e];
+        }
+        return max_flow;
+    }
+};
+constexpr int maximum_flow_push_relabel::nil, maximum_flow_push_relabel::inf;
--- code/test/maximum_flow.cpp
+++ code/test/maximum_flow.cpp
@@ -2,20 +2,19 @@
 
 // *****
 
+template <typename MF>
 void test() {
-    maximum_flow mf(4);
+    MF mf(5);
     mf.add(1, 2, 1000);
     mf.add(1, 3, 1000);
     mf.add(2, 3, 1);
     mf.add(2, 4, 1000);
     mf.add(3, 4, 1000);
-    mf.add_source(1);
-    mf.add_sink(4);
-    int max_flow = mf.compute();
+    int max_flow = mf.compute(1, 4);
     printf("max flow #1: %d\n", max_flow);
     assert(max_flow == 2000);
 
-    mf = maximum_flow(7);
+    mf = MF(8);
     mf.add(1, 2, 3);
     mf.add(1, 4, 3);
     mf.add(2, 3, 4);
@@ -27,14 +26,27 @@ void test() {
     mf.add(5, 2, 1);
     mf.add(5, 7, 1);
     mf.add(6, 7, 9);
-    mf.add_source(1);
-    mf.add_sink(7);
-    max_flow = mf.compute();
+    max_flow = mf.compute(1, 7);
     printf("max flow #2: %d\n", max_flow);
     assert(max_flow == 5);
+
+    mf = MF(7);
+    mf.add(1, 2, 15);
+    mf.add(1, 4, 4);
+    mf.add(2, 3, 12);
+    mf.add(2, 6, 7);
+    mf.add(3, 4, 3);
+    mf.add(4, 5, 10);
+    mf.add(5, 2, 5);
+    mf.add(5, 6, 10);
+    max_flow = mf.compute(1, 6);
+    printf("max flow #3: %d\n", max_flow);
+    assert(max_flow == 14);
 }
 
 int main() {
-    test();
+    test<maximum_flow>();
+    test<maximum_flow_dinic>();
+    test<maximum_flow_push_relabel>();
     return 0;
 }
