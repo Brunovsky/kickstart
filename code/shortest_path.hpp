6bdc2f4b2ecba122f3bcda91ca5dc6a059cf7fca
Bruno Carvalho <elbrunovsky@gmail.com>
2020-07-28 23:50:48 +0100

    code: Add shortest path templates

---
+++ code/shortest_path.hpp
@@ -0,0 +1,271 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// *****
+
+/**
+ * Implementation for directed graphs
+ * For undirected graphs insert edges both ways
+ */
+struct dijkstra {
+    int V, E;
+    vector<vector<pair<int, int>>> adj;
+
+    dijkstra(int V) : V(V), E(0) {
+        adj.resize(V, {});
+    }
+
+    void add(int u, int v, int w) {
+        assert(0 <= u && u < V && 0 <= v && v < V && u != v && w >= 0);
+        adj[u].push_back({v, w});
+    }
+
+    vector<int> dist;
+    vector<int> prev;
+
+    void compute(int s, int t = -1) {
+        dist.assign(V, INT_MAX);
+        prev.assign(V, -1);
+        dist[s] = 0;
+        prev[s] = s;
+
+        using int2 = pair<int, int>;
+        vector<bool> vis(V, false);
+        priority_queue<int2, vector<int2>, greater<int2>> Q;
+        Q.push({0, s});
+
+        while (!Q.empty()) {
+            int u = Q.top().second;
+            Q.pop();
+            if (vis[u]) {
+                continue;
+            } else if (u == t) {
+                break;
+            }
+            vis[u] = true;
+            for (auto edge : adj[u]) {
+                int v = edge.first, w = edge.second;
+                int cost = dist[u] + w;
+                if (!vis[v] && cost < dist[v]) {
+                    dist[v] = cost;
+                    prev[v] = u;
+                    Q.push({cost, v});
+                }
+            }
+        }
+    }
+
+    auto path(int v) {
+        vector<int> path;
+        if (prev[v] == -1) {
+            return path;
+        }
+        int t;
+        do {
+            path.push_back(v);
+            t = v;
+            v = prev[v];
+        } while (t != v);
+        reverse(begin(path), end(path));
+        return path;
+    }
+};
+
+/**
+ * Implementation for directed graphs
+ * For undirected graphs insert edges both ways
+ */
+struct astar {
+    int V, E;
+    vector<vector<pair<int, int>>> adj;
+    using heuristic_t = const function<int(int)>&;
+
+    astar(int V) : V(V), E(0) {
+        adj.resize(V, {});
+    }
+
+    void add(int u, int v, int w) {
+        assert(0 <= u && u < V && 0 <= v && v < V && u != v && w >= 0);
+        adj[u].push_back({v, w});
+    }
+
+    vector<int> dist;
+    vector<int> prev;
+
+    int compute(int s, int t, heuristic_t h) {
+        dist.assign(V, INT_MAX);
+        prev.assign(V, -1);
+        dist[s] = 0;
+        prev[s] = s;
+
+        using int2 = pair<int, int>;
+        vector<bool> vis(V, false);
+        priority_queue<int2, vector<int2>, greater<int2>> Q;
+        Q.push({0, s});
+
+        while (!Q.empty()) {
+            int u = Q.top().second;
+            Q.pop();
+            if (vis[u]) {
+                continue;
+            } else if (u == t) {
+                break;
+            }
+            vis[u] = true;
+            for (auto edge : adj[u]) {
+                int v = edge.first, w = edge.second;
+                int cost = dist[u] + w;
+                if (!vis[v] && cost < dist[v]) {
+                    dist[v] = cost;
+                    prev[v] = u;
+                    Q.push({cost + h(v), v});
+                }
+            }
+        }
+
+        return dist[t];
+    }
+
+    auto path(int v) {
+        vector<int> path;
+        if (prev[v] == -1) {
+            return path;
+        }
+        int t;
+        do {
+            path.push_back(v);
+            t = v;
+            v = prev[v];
+        } while (t != v);
+        reverse(begin(path), end(path));
+        return path;
+    }
+};
+
+/**
+ * Implementation for directed graphs
+ * For undirected graphs insert edges both ways
+ */
+struct floyd_warshall {
+    int V, E;
+    vector<int> source;
+    vector<int> target;
+    vector<int> weight;
+
+    floyd_warshall(int V) : V(V), E(0) {}
+
+    void add(int u, int v, int w) {
+        assert(0 <= u && u < V && 0 <= v && v < V && u != v && w >= 0);
+        source.push_back(u);
+        target.push_back(v);
+        weight.push_back(w);
+        E++;
+    }
+
+    vector<vector<int>> dist;
+    vector<vector<int>> next;
+
+    void compute() {
+        dist.assign(V, vector<int>(V, INT_MAX));
+        next.assign(V, vector<int>(V, -1));
+
+        for (int e = 0; e < E; e++) {
+            int u = source[e], v = target[e];
+            dist[u][v] = weight[e];
+            next[u][v] = v;
+        }
+        for (int u = 0; u < V; u++) {
+            dist[u][u] = 0;
+            next[u][u] = u;
+        }
+
+        bool stop = false;
+        for (int k = 0; k < V && !stop; k++) {
+            stop = true;
+            for (int u = 0; u < V; u++) {
+                for (int v = 0; v < V; v++) {
+                    if (dist[u][v] > dist[u][k] + dist[k][v]) {
+                        dist[u][v] = dist[u][k] + dist[k][v];
+                        next[u][v] = next[u][k];
+                        stop = false;
+                    }
+                }
+            }
+        }
+    }
+
+    auto path(int u, int v) {
+        vector<int> path;
+        if (next[u][v] == -1) {
+            return path;
+        }
+        do {
+            path.push_back(u);
+            u = next[u][v];
+        } while (u != v);
+        return path;
+    }
+};
+
+/**
+ * For directed graphs with negative weights
+ */
+struct bellman_ford {
+    int V, E;
+    vector<int> source;
+    vector<int> target;
+    vector<int> weight;
+
+    bellman_ford(int V) : V(V), E(0) {}
+
+    void add(int u, int v, int w) {
+        assert(0 <= u && u < V && 0 <= v && v < V && u != v);
+        source.push_back(u);
+        target.push_back(v);
+        weight.push_back(w);
+        E++;
+    }
+
+    vector<int> dist;
+    vector<int> prev;
+
+    void compute(int s) {
+        dist.assign(V, INT_MAX);
+        prev.assign(V, -1);
+        dist[s] = 0;
+
+        bool stop = false;
+        for (int k = 1; k < V && !stop; k++) {
+            stop = true;
+            for (int e = 0; e < E; e++) {
+                int u = source[e], v = target[e];
+                if (dist[u] < dist[v] + weight[e]) {
+                    dist[u] = dist[v] + weight[e];
+                    prev[v] = u;
+                    stop = false;
+                }
+            }
+        }
+
+        for (int e = 0; e < E; e++) {
+            int u = source[e], v = target[e];
+            assert(dist[u] + weight[e] >= dist[v]);
+        }
+    }
+
+    auto path(int v) {
+        vector<int> path;
+        if (prev[v] == -1) {
+            return path;
+        }
+        int t;
+        do {
+            path.push_back(v);
+            t = v;
+            v = prev[v];
+        } while (t != v);
+        reverse(begin(path), end(path));
+        return path;
+    }
+};
---
+++ code/test/shortest_path.cpp
@@ -0,0 +1,56 @@
+#include "../shortest_path.hpp"
+
+using namespace std;
+
+// *****
+
+// https://en.wikipedia.org/wiki/Shortest_path_problem#/media/File:Shortest_path_with_direct_weights.svg
+template <typename MM>
+void add_g1(MM& mm) {
+    mm = MM(6);
+    mm.add(0, 1, 4);
+    mm.add(0, 2, 2);
+    mm.add(1, 2, 5);
+    mm.add(1, 3, 10);
+    mm.add(2, 4, 3);
+    mm.add(3, 5, 11);
+    mm.add(4, 3, 4);
+}
+
+template <typename MM>
+void add_g2(MM& mm) {
+    mm = MM(7);
+    mm.add(1, 2, 7);
+    mm.add(1, 3, 9);
+    mm.add(1, 6, 14);
+    mm.add(2, 3, 10);
+    mm.add(2, 4, 15);
+    mm.add(3, 4, 11);
+    mm.add(3, 6, 2);
+    mm.add(4, 5, 6);
+    mm.add(5, 6, 9);
+    mm.add(6, 5, 9); // double
+}
+
+void test() {
+    dijkstra mm(6);
+    add_g1(mm);
+    mm.compute(0);
+    printf("dist[0..3]: %d\tdist[0..5]: %d\n", mm.dist[3], mm.dist[5]);
+    assert(mm.dist[3] == 9);
+    assert(mm.dist[5] == 20);
+    assert(mm.path(5) == vector<int>({0, 2, 4, 3, 5}));
+
+    add_g2(mm);
+    mm.compute(1);
+    printf("dist[0..5]: %d\tdist[0..6]: %d\n", mm.dist[5], mm.dist[6]);
+    assert(mm.dist[0] == INT_MAX);
+    assert(mm.dist[5] == 20);
+    assert(mm.dist[6] == 11);
+    assert(mm.path(5) == vector<int>({1, 3, 6, 5}));
+}
+
+int main() {
+    test();
+    return 0;
+}
