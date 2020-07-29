ec93f31515d5dab77b135b6046626d477e36e7f1
Bruno Carvalho <elbrunovsky@gmail.com>
2020-07-29 01:05:31 +0100

    code: Test hash and add rolling hasher

--- code/hash.hpp
+++ code/hash.hpp
@@ -4,8 +4,8 @@ using namespace std;
 
 // *****
 
-struct vechasher {
-    template <template <typename...> typename Container, typename V, typename... T>
+struct vec_hasher {
+    template <template <typename...> class Container, typename V, typename... T>
     size_t operator()(const Container<V, T...>& vec) const noexcept {
         hash<V> hasher;
         size_t seed = distance(begin(vec), end(vec));
@@ -16,10 +16,44 @@ struct vechasher {
     }
 };
 
-struct pairhasher {
+struct pair_hasher {
     template <typename U, typename V>
     size_t operator()(const pair<U, V>& p) const noexcept {
         size_t lhs = p.first, rhs = p.second;
         return (lhs + rhs) * (lhs + rhs + 1) / 2 + rhs;
     }
 };
+
+constexpr size_t powmod(size_t base, size_t n, size_t mod) {
+    size_t power = 1;
+    while (n) {
+        if (n & 1) {
+            power = power * base % mod;
+        }
+        n >>= 1;
+        base = base * base % mod;
+    }
+    return power;
+}
+
+template <size_t n>
+struct rolling_hasher {
+    static constexpr size_t base = 24'230'861UL;
+    static constexpr size_t mod = 189'326'923UL;
+    static constexpr size_t mul = powmod(base, n, mod);
+
+    size_t operator()(const string& s) const noexcept {
+        assert(s.size() == n);
+        size_t seed = 0;
+        for (unsigned char c : s) {
+            seed = (seed * base + c) % mod;
+        }
+        return seed;
+    }
+
+    size_t roll(size_t seed, unsigned char out, unsigned char in) const noexcept {
+        return (seed * base + in + (mod - out) * mul) % mod;
+    }
+};
+
+// 9 8 7 6 5 4 3 2 1
---
+++ code/test/hash.cpp
@@ -0,0 +1,29 @@
+#include "../hash.hpp"
+
+using namespace std;
+
+// *****
+
+void test() {
+    rolling_hasher<10> hasher;
+    string s1 = "aababbcacc";
+    string s2 = "ababbcaccd";
+    string s3 = "abbcaccdef";
+    size_t h1 = hasher(s1);
+    size_t h2 = hasher(s2);
+    size_t h3 = hasher(s3);
+    printf("hash(%s): %zu\n", s1.data(), h1);
+    printf("hash(%s): %zu\n", s2.data(), h2);
+    printf("hash(%s): %zu\n", s3.data(), h3);
+    size_t h2b = hasher.roll(h1, 'a', 'd');
+    size_t h3b = hasher.roll(hasher.roll(h2b, 'a', 'e'), 'b', 'f');
+    printf("roll(s2): %zu\n", h2b);
+    printf("roll(s3): %zu\n", h3b);
+    assert(h2 == h2b);
+    assert(h3 == h3b);
+}
+
+int main() {
+    test();
+    return 0;
+}
