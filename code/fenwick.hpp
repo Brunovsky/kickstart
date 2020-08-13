f9c3eb51896074c67a7f70a521f40a24379fa73f
Bruno Carvalho <elbrunovsky@gmail.com>
2020-08-13 12:43:41 +0100

    code: fenwick: Add tests and template struct wrappers

--- code/fenwick.hpp
+++ code/fenwick.hpp
@@ -4,63 +4,80 @@ using namespace std;
 
 // *****
 
-#define MAXN 100001
+template <int MAXN>
+struct fenwick {
+    int N;
+    int tree[MAXN + 1]{};
 
-int N;
-int fenwick[MAXN];
+    fenwick() {}
+    fenwick(int N) : N(N) {}
 
-int sum(int i) {
-    int sum = 0;
-    while (i > 0) {
-        sum += fenwick[i];
-        i -= i & -i;
+    int sum(int i) {
+        int sum = 0;
+        while (i > 0) {
+            sum += tree[i];
+            i -= i & -i;
+        }
+        return sum;
     }
-    return sum;
-}
 
-void add(int i, int n) {
-    assert(i > 0);
-    while (i <= N) {
-        fenwick[i] += n;
-        i += i & -i;
+    void add(int i, int n) {
+        assert(i > 0);
+        while (i <= N) {
+            tree[i] += n;
+            i += i & -i;
+        }
     }
-}
+
+    int lower_bound(int n) {
+        int i = 0;
+        int bits = CHAR_BIT * sizeof(int) - __builtin_clz(N << 1);
+        for (int j = 1 << bits; j; j >>= 1) {
+            if (i + j <= N && tree[i + j] < n) {
+                n -= tree[i + j];
+                i += j;
+            }
+        }
+        return i + 1;
+    }
+};
 
 // *****
 
-#define MAXM 500
+template <int MAXN, int MAXM>
+struct fenwick2d {
+    int N, M;
+    int tree[MAXN + 1][MAXM + 1]{};
 
-int M;
-int fenwick2d[MAXN][MAXM];
+    fenwick2d() {}
+    fenwick2d(int N, int M) : N(N), M(M) {}
 
-int sum2(int i, int j) {
-    int sum = 0;
-    while (i > 0) {
-        int k = j;
-        while (k > 0) {
-            sum += fenwick2d[i][k];
-            k -= k & -k;
+    int sum(int i, int j) {
+        int sum = 0;
+        while (i > 0) {
+            int k = j;
+            while (k > 0) {
+                sum += tree[i][k];
+                k -= k & -k;
+            }
+            i -= i & -i;
         }
-        i -= i & -i;
+        return sum;
     }
-    return sum;
-}
 
-void add2(int i, int j, int n) {
-    assert(i > 0 && j > 0);
-    while (i <= N) {
-        int k = j;
-        while (k <= N) {
-            fenwick2d[i][k] += n;
-            k += k & -k;
+    void add(int i, int j, int n) {
+        assert(i > 0 && j > 0);
+        while (i <= N) {
+            int k = j;
+            while (k <= M) {
+                tree[i][k] += n;
+                k += k & -k;
+            }
+            i += i & -i;
         }
-        i += i & -i;
     }
-}
+};
 
 // *****
 
-void driver() {
-    memset(fenwick, 0, sizeof(fenwick));
-    memset(fenwick2d, 0, sizeof(fenwick2d));
-}
+void driver() { fenwick<30000> fw(30000); }
---
+++ code/test/fenwick.cpp
@@ -0,0 +1,51 @@
+#include "../fenwick.hpp"
+
+// *****
+
+void test() {
+    fenwick<100> fw(100);
+
+    fw.add(10, 10);
+    fw.add(20, 20);
+    fw.add(30, 30);
+    fw.add(50, 100);
+    fw.add(73, 80);
+    fw.add(100, 300);
+    assert(fw.sum(29) == 30);
+    assert(fw.sum(30) == 60);
+    assert(fw.sum(40) == 60);
+    assert(fw.sum(70) == 160);
+    assert(fw.sum(80) == 240);
+    assert(fw.sum(100) == 540);
+    assert(fw.lower_bound(30) == 20);
+    assert(fw.lower_bound(31) == 30);
+    assert(fw.lower_bound(159) == 50);
+    assert(fw.lower_bound(540) == 100);
+    assert(fw.lower_bound(541) == 101); // end is past N
+}
+
+void test2d() {
+    fenwick2d<100, 100> fw(100, 100);
+
+    fw.add(10, 10, 10);
+    fw.add(20, 20, 20);
+    fw.add(30, 50, 40);
+    fw.add(50, 30, 40);
+    fw.add(80, 80, 100);
+    fw.add(5, 95, 60);
+    fw.add(85, 5, 60);
+
+    assert(fw.sum(40, 40) == 30);
+    assert(fw.sum(10, 90) == 10);
+    assert(fw.sum(40, 90) == 70);
+    assert(fw.sum(70, 90) == 110);
+    assert(fw.sum(80, 90) == 210);
+    assert(fw.sum(80, 94) == 210);
+    assert(fw.sum(80, 95) == 270);
+}
+
+int main() {
+    test();
+    test2d();
+    return 0;
+}
