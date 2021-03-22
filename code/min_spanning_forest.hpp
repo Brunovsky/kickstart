b844052995aa8e4b618842573ee467cbc182dc8e
Bruno Carvalho <elbrunovsky@gmail.com>
2021-03-22 18:52:12 +0000

    code: msf: Fix style, standardize tests

--- code/min_spanning_forest.hpp
+++ code/min_spanning_forest.hpp
@@ -5,44 +5,62 @@
 
 // *****
 
-/**
- * Kruskal's algorithm for minimum spanning forest
- * Complexity: O(E log E)
- */
-struct min_spanning_forest {
-    int V, E = 0;
-    vector<int> source, target;
-    vector<long> weight;
+using edges_t = vector<array<int, 2>>;
+using weights_t = vector<long>;
 
-    explicit min_spanning_forest(int V) : V(V) {}
+long min_spanning_forest_kruskal(int V, const edges_t& g, const weights_t& weight) {
+    int E = g.size();
+    disjoint_set set(V);
+    vector<int> edges(E);
+    iota(begin(edges), end(edges), 0);
+    sort(begin(edges), end(edges),
+         [&weight](int e1, int e2) { return weight[e1] < weight[e2]; });
 
-    void add(int u, int v, long w) {
-        assert(0 <= u && u < V && 0 <= v && v < V && u != v);
-        source.push_back(u);
-        target.push_back(v);
-        weight.push_back(w);
-        E++;
-    }
-
-    int compute() {
-        disjoint_set set(V);
-        vector<int> edges(E);
-        iota(begin(edges), end(edges), 0);
-        sort(begin(edges), end(edges), [this](int e1, int e2) {
-            return weight[e1] < weight[e2];
-        });
-
-        int msf_weight = 0;
-        for (int e : edges) {
-            int u = source[e], v = target[e];
-            if (set.find(u) != set.find(v)) {
-                msf_weight += weight[e];
-                set.join(u, v);
-                // add e to a vec if you want to output the tree's edges
-            }
+    long msf = 0;
+    for (int e : edges) {
+        auto [u, v] = g[e];
+        if (set.find(u) != set.find(v)) {
+            msf += weight[e];
+            set.join(u, v);
         }
-        return msf_weight;
     }
-};
+    return msf;
+}
+
+long min_spanning_forest_prim(int V, const edges_t& g, const weights_t& weight) {
+    int E = g.size(), e = 0;
+    vector<int> adj(2 * E);
+    vector<int> off(V + 1, 0);
+    for (auto [u, v] : g)
+        off[u + 1]++, off[v + 1]++;
+    inclusive_scan(begin(off), end(off), begin(off));
+    auto cur = off;
+    for (auto [u, v] : g)
+        adj[cur[u]++] = adj[cur[v]++] = e++;
+
+    vector<bool> vis(V, false);
+    long msf = 0;
+    for (int u = 0; u < V; u++) {
+        if (vis[u])
+            continue;
+
+        priority_queue<pair<long, int>> Q;
+        Q.push({0, u});
+
+        while (!Q.empty()) {
+            auto [nw, u] = Q.top();
+            Q.pop();
+            if (vis[u])
+                continue;
+
+            vis[u] = true;
+            msf += -nw;
+            for (int i = off[u]; i < off[u + 1]; i++)
+                if (int e = adj[i], v = g[e][u != g[e][1]]; !vis[v])
+                    Q.push({-weight[e], v});
+        }
+    }
+    return msf;
+}
 
 #endif // MIN_SPANNING_FOREST_HPP
---
+++ code/test/min_spanning_forest.cpp
@@ -0,0 +1,22 @@
+#include "../min_spanning_forest.hpp"
+
+#include "test_utils.hpp"
+
+// *****
+
+void unit_test_min_spanning_forest() {
+    edges_t g;
+    weights_t weight;
+
+    g = {{1, 2}, {1, 4}, {1, 5}, {2, 4}, {2, 5}, {3, 5}, {3, 6}, {4, 5}, {5, 6}};
+    weight = {1, 4, 3, 4, 2, 4, 5, 4, 7};
+    long w0 = min_spanning_forest_kruskal(7, g, weight);
+    long w1 = min_spanning_forest_prim(7, g, weight);
+    assert(w0 == 16 && w1 == 16);
+    print_ok("min spanning forest");
+}
+
+int main() {
+    unit_test_min_spanning_forest();
+    return 0;
+}
