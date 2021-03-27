7e548676fababf42b1f4d8e7b9be46b6f5e0feb2
Bruno Carvalho <elbrunovsky@gmail.com>
2021-03-27 23:04:05 +0000

    code: debug_print: Reorder for easier copy

--- code/debug_print.hpp
+++ code/debug_print.hpp
@@ -39,9 +39,18 @@ template <typename U, typename V>
 string to_string(const pair<U, V>& uv) {
     return '(' + to_string(uv.first) + ',' + to_string(uv.second) + ')';
 }
+template <typename U, typename V>
+ostream& operator<<(ostream& out, const pair<U, V>& v) {
+    return out << to_string(v);
+}
+
 template <typename T>
 string to_string(const array<T, 2>& uv) {
     return '(' + to_string(uv[0]) + ',' + to_string(uv[1]) + ')';
+}
+template <typename T, size_t N>
+ostream& operator<<(ostream& out, const array<T, N>& v) {
+    return out << to_string(v);
 }
 
 template <typename T, size_t N>
@@ -49,78 +58,75 @@ string to_string(const array<T, N>& v) {
     string s = " ";
     for (const auto& el : v)
         s += to_string(el) + " ";
-    return '[' + s + ']';
+    return s;
 }
+
 template <typename T>
 string to_string(const vector<T>& v) {
     string s = " ";
     for (const auto& el : v)
         s += to_string(el) + " ";
-    return '[' + s + ']';
-}
-template <typename T>
-string to_string(const list<T>& v) {
-    string s = " ";
-    for (const auto& el : v)
-        s += to_string(el) + " ";
-    return '(' + s + ')';
-}
-template <typename T>
-string to_string(const set<T>& v) {
-    string s = " ";
-    for (const auto& el : v)
-        s += to_string(el) + " ";
-    return '{' + s + '}';
-}
-template <typename T>
-string to_string(const unordered_set<T>& v) {
-    string s = " ";
-    for (const auto& el : v)
-        s += to_string(el) + " ";
-    return '{' + s + '}';
-}
-template <typename K, typename V>
-string to_string(const map<K, V>& v) {
-    string s = " ";
-    for (const auto& el : v)
-        s += to_string(el) + " ";
-    return '{' + s + '}';
-}
-template <typename K, typename V>
-string to_string(const unordered_map<K, V>& v) {
-    string s = " ";
-    for (const auto& el : v)
-        s += to_string(el) + " ";
-    return '{' + s + '}';
-}
-
-template <typename U, typename V>
-ostream& operator<<(ostream& out, const pair<U, V>& v) {
-    return out << to_string(v);
-}
-template <typename T, size_t N>
-ostream& operator<<(ostream& out, const array<T, N>& v) {
-    return out << to_string(v);
+    return s;
 }
 template <typename T>
 ostream& operator<<(ostream& out, const vector<T>& v) {
     return out << to_string(v);
 }
+
+template <typename T>
+string to_string(const list<T>& v) {
+    string s = " ";
+    for (const auto& el : v)
+        s += to_string(el) + " ";
+    return s;
+}
 template <typename T>
 ostream& operator<<(ostream& out, const list<T>& v) {
     return out << to_string(v);
+}
+
+template <typename T>
+string to_string(const set<T>& v) {
+    string s = " ";
+    for (const auto& el : v)
+        s += to_string(el) + " ";
+    return s;
 }
 template <typename T>
 ostream& operator<<(ostream& out, const set<T>& v) {
     return out << to_string(v);
 }
+
+template <typename T>
+string to_string(const unordered_set<T>& v) {
+    string s = " ";
+    for (const auto& el : v)
+        s += to_string(el) + " ";
+    return s;
+}
 template <typename T>
 ostream& operator<<(ostream& out, const unordered_set<T>& v) {
     return out << to_string(v);
 }
+
+template <typename K, typename V>
+string to_string(const map<K, V>& v) {
+    string s = " ";
+    for (const auto& el : v)
+        s += to_string(el) + " ";
+    return s;
+}
 template <typename K, typename V>
 ostream& operator<<(ostream& out, const map<K, V>& v) {
     return out << to_string(v);
+}
+
+template <typename K, typename V>
+string to_string(const unordered_map<K, V>& v) {
+    string s = " ";
+    for (const auto& el : v)
+        s += to_string(el) + " ";
+    return s;
 }
 template <typename K, typename V>
 ostream& operator<<(ostream& out, const unordered_map<K, V>& v) {
--- problems/codejam/2016/1B-getting-the-digits/README.md
+++ problems/codejam/2016/1B-getting-the-digits/README.md
@@ -2,8 +2,8 @@
 
 ## [Getting the Digits (11pts, 12pts)](https://codingcompetitions.withgoogle.com/codejam/round/0000000000201b6c/0000000000201bf0)
 
-Unattempted
+Solved
 
-* Time: 0 hours
-* Complexity: -
+* Time: 10 minutes
+* Complexity: O(1)
 * Memory: -
