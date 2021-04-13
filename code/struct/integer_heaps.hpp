82873fc8b4134c19fe46616e76b6dcd139e5d5c9
Bruno Carvalho <bruno.carvalho.feup@gmail.com>
2021-04-13 01:45:13 +0100

    code: heaps: Shift indices

--- code/struct/integer_heaps.hpp
+++ code/struct/integer_heaps.hpp
@@ -93,25 +93,35 @@ struct binary_int_heap {
  *           O(log n)*       pop()
  *           O(log n)*       erase(u)     not implemented
  *           O(n)            clear()
+ *
+ * Usage:
+ *      vector<int> dist(V); ...
+ *      pairing_int_heap heap(V, greater_container(dist));
+ *      heap.push(0);                  -- push 0 onto heap
+ *      heap.push_or_improve(1);       -- dec. key of 1 (or push)
+ *      heap.improve(0);               -- dec. key of 0, 0 must've been pushed
+ *      int v = heap.top();            -- top of heap
+ *      heap.clear();                  -- clear the whole heap (not recommended)
+ *      v = heap.pop(0);               -- pop from heap (returns value)
  */
 template <typename Compare = less<>>
 struct pairing_int_heap {
     struct node_t {
-        int parent = -2, child = -1, next = -1, prev = -1;
+        int parent = 0, child = 0, next = 0, prev = 0;
     };
     vector<node_t> node;
-    int root = -1;
+    int root = 0;
     Compare comp;
 
     explicit pairing_int_heap(int N = 0, const Compare& comp = Compare())
-        : node(N), comp(comp) {}
+        : node(N + 1), comp(comp) {}
 
-    bool empty() const { return root == -1; }
-    bool contains(int u) const { return node[u].parent != -2; }
-    int top() const { return assert(!empty()), root; }
+    bool empty() const { return root == 0; }
+    bool contains(int u) const { return u++, node[u].parent != 0; }
+    int top() const { return root - 1; }
 
     void push(int u) {
-        assert(!contains(u));
+        assert(!contains(u)), u++;
         node[u].parent = -1;
         root = empty() ? u : meld(root, u);
     }
@@ -120,53 +130,55 @@ struct pairing_int_heap {
         int u = root;
         root = two_pass_pairing(u);
         node[u] = node_t();
-        return u;
+        return u - 1;
     }
     void improve(int u) {
-        assert(!empty() && contains(u));
-        if (u != root && comp(u, node[u].parent))
+        assert(!empty() && contains(u)), u++;
+        if (u != root && do_comp(u, node[u].parent)) {
             take(u), root = meld(root, u);
+        }
     }
     void push_or_improve(int u) { contains(u) ? improve(u) : push(u); }
+
     void clear() {
-        if (root != -1)
-            clear_dive(root), root = -1;
+        if (!empty()) {
+            clear_dive(root), root = 0;
+        }
     }
 
   private:
-    int meld(int u, int v) { return comp(u, v) ? splice(u, v) : splice(v, u); }
+    bool do_comp(int u, int v) const { return comp(u - 1, v - 1); }
+    int meld(int u, int v) { return do_comp(u, v) ? splice(u, v) : splice(v, u); }
     int splice(int u, int v) {
-        if (node[u].child != -1)
-            node[node[u].child].prev = v;
+        node[node[u].child].prev = v;
         node[v].next = node[u].child, node[u].child = v;
         return node[v].prev = node[v].parent = u;
     }
     void take(int u) {
-        assert(node[u].parent != -1);
+        // assert(node[u].parent > 0);
         if (node[node[u].parent].child == u)
             node[node[u].parent].child = node[u].next;
         else
             node[node[u].prev].next = node[u].next;
-        if (node[u].next != -1)
-            node[node[u].next].prev = node[u].prev;
+        node[node[u].next].prev = node[u].prev;
     }
     int two_pass_pairing(int n) {
-        if (node[n].child == -1)
-            return -1;
+        if (node[n].child == 0)
+            return 0;
         int u = node[n].child, v = node[u].next, w;
-        while (v != -1 && node[v].next != -1) {
+        while (v && node[v].next) {
             w = node[node[v].next].next;
             u = node[u].next = v = meld(v, node[v].next);
             v = node[v].next = w;
         }
         u = node[n].child, v = node[u].next;
-        while (v != -1) {
+        while (v) {
             w = node[v].next, u = meld(u, v), v = w;
         }
         return u;
     }
     void clear_dive(int u) {
-        for (int v = node[u].child; v != -1; v = node[v].next)
+        for (int v = node[u].child; v; v = node[v].next)
             clear_dive(v);
         node[u] = node_t();
     }
@@ -184,27 +196,37 @@ struct pairing_int_heap {
  *           O(1)            merge(h,g)
  *           O(log log n)*   decrease_key(h,u)
  *           O(log n)*       pop(h)
- *           O(log n)*       erase(n)     not implemented
- *           O(n)            clear(h)     not implemented
+ *           O(n)            clear(h)
+ *
+ * Usage:
+ *      vector<int> dist(V); ...
+ *      pairing_int_heaps heaps(N, V, greater_container(dist));
+ *      heaps.push(1, 0);               -- push 0 onto heap 1
+ *      heaps.push_or_improve(2, 1);    -- dec. key of 1 in heap 2 (or push)
+ *      heaps.improve(1, 0);            -- dec. key of 0 in heap 1, 0 must've been pushed
+ *      int v = heaps.top(0);           -- top of heap 0
+ *      heaps.merge(1, 2);              -- merge heap 2 into heap 1 (then heap 2 is empty)
+ *      heaps.clear(1);                 -- clear heap 1 (not recommended)
+ *      v = heaps.pop(0);               -- pop from heap 0 (returns value)
  */
 template <typename Compare = less<>>
 struct pairing_int_heaps {
     struct node_t {
-        int parent = -2, child = -1, next = -1, prev = -1;
+        int parent = 0, child = 0, next = 0, prev = 0;
     };
     vector<int> root;
     vector<node_t> node;
     Compare comp;
 
     explicit pairing_int_heaps(int R = 0, int N = 0, const Compare& comp = Compare())
-        : root(R, -1), node(N), comp(comp) {}
+        : root(R, 0), node(N + 1), comp(comp) {}
 
-    bool empty(int h) const { return root[h] == -1; }
-    bool contains(int u) const { return node[u].parent != -2; }
-    int top(int h) const { return assert(!empty(h)), root[h]; }
+    bool empty(int h) const { return root[h] == 0; }
+    bool contains(int u) const { return u++, node[u].parent != 0; }
+    int top(int h) const { return root[h] - 1; }
 
     void push(int h, int u) {
-        assert(!contains(u));
+        assert(!contains(u)), u++;
         node[u].parent = -1;
         root[h] = empty(h) ? u : meld(root[h], u);
     }
@@ -213,57 +235,61 @@ struct pairing_int_heaps {
         int u = root[h];
         root[h] = two_pass_pairing(u);
         node[u] = node_t();
-        return u;
+        return u - 1;
     }
     void improve(int h, int u) {
-        assert(!empty(h) && contains(u));
-        if (u != root[h] && comp(u, node[u].parent))
+        assert(!empty(h) && contains(u)), u++;
+        if (u != root[h] && do_comp(u, node[u].parent)) {
             take(u), root[h] = meld(root[h], u);
+        }
     }
     void push_or_improve(int h, int u) { contains(u) ? improve(h, u) : push(h, u); }
+
     void merge(int h, int g) {
-        if (h != g && !empty(g))
-            root[h] = empty(h) ? root[g] : meld(root[h], root[g]), root[g] = -1;
+        if (h != g && !empty(g)) {
+            root[h] = empty(h) ? root[g] : meld(root[h], root[g]);
+            root[g] = 0;
+        }
     }
     void clear(int h) {
-        if (root[h] != -1)
-            clear_dive(root[h]), root[h] = -1;
+        if (!empty(h)) {
+            clear_dive(root[h]), root[h] = 0;
+        }
     }
 
   private:
-    int meld(int u, int v) { return comp(u, v) ? splice(u, v) : splice(v, u); }
+    bool do_comp(int u, int v) const { return comp(u - 1, v - 1); }
+    int meld(int u, int v) { return do_comp(u, v) ? splice(u, v) : splice(v, u); }
     int splice(int u, int v) {
-        if (node[u].child != -1)
-            node[node[u].child].prev = v;
+        node[node[u].child].prev = v;
         node[v].next = node[u].child, node[u].child = v;
         return node[v].prev = node[v].parent = u;
     }
     void take(int u) {
-        assert(node[u].parent != -1);
+        assert(node[u].parent > 0);
         if (node[node[u].parent].child == u)
             node[node[u].parent].child = node[u].next;
         else
             node[node[u].prev].next = node[u].next;
-        if (node[u].next != -1)
-            node[node[u].next].prev = node[u].prev;
+        node[node[u].next].prev = node[u].prev;
     }
     int two_pass_pairing(int n) {
-        if (node[n].child == -1)
-            return -1;
+        if (node[n].child == 0)
+            return 0;
         int u = node[n].child, v = node[u].next, w;
-        while (v != -1 && node[v].next != -1) {
+        while (v && node[v].next) {
             w = node[node[v].next].next;
             u = node[u].next = v = meld(v, node[v].next);
             v = node[v].next = w;
         }
         u = node[n].child, v = node[u].next;
-        while (v != -1) {
+        while (v) {
             w = node[v].next, u = meld(u, v), v = w;
         }
         return u;
     }
     void clear_dive(int u) {
-        for (int v = node[u].child; v != -1; v = node[v].next)
+        for (int v = node[u].child; v; v = node[v].next)
             clear_dive(v);
         node[u] = node_t();
     }
