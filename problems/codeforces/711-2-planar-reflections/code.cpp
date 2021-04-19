455d0de2b108900b2a49358e658140c61331f739
Bruno Carvalho <bruno.carvalho.feup@gmail.com>
2021-04-19 20:52:03 +0100

    codeforces: 711 Planar Reflections

--- problems/codeforces/711-2-planar-reflections/code.cpp
+++ problems/codeforces/711-2-planar-reflections/code.cpp
@@ -4,27 +4,50 @@ using namespace std;
 
 // *****
 
-constexpr size_t MAXN = 1001, MAXK = 1001;
-size_t dp[MAXN][MAXK];
+// ->     |  |  |  |  |  |  |  |
+//  0     1  2  3  4  5  6  7  N
+// dp[n]: number of D(k) leaving plane n, 1<=n<=N
+// prefix[n]: number of D(k) leaving plane [1..n)
+// suffix[n]: number of D(k) leaving plane [n+1...)
 
-void setup() {
-    for (int n = 0; n < MAXN; n++)
-        dp[n][0] = 0, dp[n][1] = 1;
+constexpr unsigned MOD = 1e9 + 7;
 
-    for (int k = 2; k < MAXK; k++) {
-        dp[0][k] = k;
-        for (int n = 1; n < MAXN; n++) {
-            dp[n][k]
+auto solve() {
+    int N, K;
+    cin >> N >> K;
+    if (K == 1)
+        return 1u;
+
+    vector<unsigned> dp(N + 1, 0), prefix(N + 2, 1), suffix(N + 2, 0);
+    bool right = true;
+    unsigned ans = 1;
+    for (int k = K - 1; k >= 1; k--) {
+        if (right) {
+            for (int n = 1; n <= N; n++) {
+                dp[n] = prefix[n];
+                ans = (ans + dp[n]) % MOD;
+            }
+        } else {
+            for (int n = 1; n <= N; n++) {
+                dp[n] = suffix[n];
+                ans = (ans + dp[n]) % MOD;
+            }
         }
+        prefix[0] = prefix[1] = suffix[N] = suffix[N + 1] = 0;
+        for (int n = 1; n <= N; n++) {
+            prefix[n + 1] = (prefix[n] + dp[n]) % MOD;
+        }
+        for (int n = N; n >= 1; n--) {
+            suffix[n - 1] = (suffix[n] + dp[n]) % MOD;
+        }
+        right = !right;
     }
+    return ans;
 }
-
-auto solve() { return 0; }
 
 // *****
 
 int main() {
-    ios::sync_with_stdio(false);
     unsigned T;
     cin >> T >> ws;
     for (unsigned t = 1; t <= T; ++t) {
