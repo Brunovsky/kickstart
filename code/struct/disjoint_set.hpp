8676b30f5ccfd703b2cd685233a3329f75f8a48e
Bruno Carvalho <bruno.carvalho.feup@gmail.com>
2021-05-16 22:58:52 +0100

    code: Add dsu with rollback

--- code/struct/disjoint_set.hpp
+++ code/struct/disjoint_set.hpp
@@ -15,7 +15,7 @@ struct disjoint_set {
 
     void assign(int N) { *this = disjoint_set(N); }
     bool same(int i, int j) { return find(i) == find(j); }
-    bool unit(int i) { return size[find(i)] == 1; }
+    bool unit(int i) { return i == next[i] && size[i] == 1; }
     bool root(int i) { return find(i) == i; }
 
     int find(int i) {
@@ -35,6 +35,52 @@ struct disjoint_set {
             next[j] = i;
             size[i] += size[j];
             S--;
+            return true;
+        }
+        return false;
+    }
+};
+
+struct disjoint_set_rollback {
+    int N, S;
+    vector<int> next;
+    vector<pair<int, int>> history;
+
+    explicit disjoint_set_rollback(int N = 0) : N(N), S(N), next(N, -1) {}
+
+    void assign(int N) { *this = disjoint_set_rollback(N); }
+    bool same(int i, int j) { return find(i) == find(j); }
+    bool unit(int i) { return next[i] == -1; }
+    bool root(int i) { return find(i) == i; }
+    int size(int i) { return -next[i]; }
+    int time() const { return history.size(); }
+
+    void rollback(int t) {
+        int i = time();
+        while (i > t) {
+            i--, S++, next[history[i].first] = history[i].second;
+        }
+        history.resize(t);
+    }
+
+    int find(int i) {
+        while (next[i] >= 0) {
+            i = next[i];
+        }
+        return i;
+    }
+
+    bool join(int i, int j) {
+        i = find(i);
+        j = find(j);
+        if (i != j) {
+            if (size(i) < size(j)) {
+                swap(i, j);
+            }
+            history.emplace_back(i, next[i]);
+            history.emplace_back(j, next[j]);
+            next[i] += next[j];
+            next[j] = i, S--;
             return true;
         }
         return false;
